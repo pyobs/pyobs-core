447cb72d3ef929bd6c4a7940597ee47c10494697
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-01-31 23:03:06 +0100

    docstrings

--- pytel/modules/test/standalone.py
+++ pytel/modules/test/standalone.py
@@ -1,18 +1,3 @@
-"""
-The StandAlone module is an example, showing the most simple module implementation without any connectivity.
-It just logs a given message continuously with a given interval.
-
-
-Configuration
--------------
-
-message (str) = Hello world
-    The message that should be logged.
-
-interval (int) = 5
-    The interval in seconds between logging messages.
-"""
-
 import logging
 
 from pytel import PytelModule
@@ -25,7 +10,12 @@ class StandAlone(PytelModule):
     """Example module without connectivity."""
 
     def __init__(self, message: str = 'Hello world', interval: int = 10, *args, **kwargs):
-        """Creates a new StandAlone object."""
+        """Creates a new StandAlone object.
+
+        Args:
+            message: Message to log in the given interval.
+            interval: Interval between messages.
+        """
         PytelModule.__init__(self, thread_funcs=self._message, *args, **kwargs)
 
         # store

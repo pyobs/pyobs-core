1e52e77cf24ff920dbb1bac6d61e9840dca97894
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-03-06 12:29:12 +0200

    Moved MotionStatus into interface

--- pytel/interfaces/IMoving.py
+++ pytel/interfaces/IMoving.py
@@ -1,31 +1,6 @@
+from enum import Enum
+
 from .IStatus import IStatus
-
-import enum
-
-
-class MotionStatus(enum.Enum):
-    """
-    Enumerator for moving device status:
-        - PARKED means that the device needs to be initialized or positioned or
-          moved (depending upon the device; some devices don't need a formal
-          initialization); presumedly, this is the safe "off" state.
-        - INITIALIZING means that the device is transitioning from a PARKED state
-          to an active state but is not yet fully operable.
-        - unPARKED is either IDLE (operating but in no particular state) or
-          POSITIONED (operating in a well-defined state)
-        - SLEWING means that the device is moving to some targeted state (e.g.
-          to POSITIONED or TRACKING) but has not yet arrived at that state
-        - TRACKING means that the device is moving as commanded
-    """
-    ABORTING = 'aborting'
-    ERROR = 'error'
-    IDLE = 'idle'
-    INITIALIZING = 'initializing'
-    PARKED = 'parked'
-    POSITIONED = 'positioned'
-    SLEWING = 'slewing'
-    TRACKING = 'tracking'
-    UNKNOWN = 'unknown'
 
 
 class IMoving(IStatus):
@@ -35,6 +10,30 @@ class IMoving(IStatus):
     There are no generic motion methods - these have to be defined in daughter
     interfaces.
     """
+
+    class Status(Enum):
+        """
+        Enumerator for moving device status:
+            - PARKED means that the device needs to be initialized or positioned or
+              moved (depending upon the device; some devices don't need a formal
+              initialization); presumedly, this is the safe "off" state.
+            - INITIALIZING means that the device is transitioning from a PARKED state
+              to an active state but is not yet fully operable.
+            - unPARKED is either IDLE (operating but in no particular state) or
+              POSITIONED (operating in a well-defined state)
+            - SLEWING means that the device is moving to some targeted state (e.g.
+              to POSITIONED or TRACKING) but has not yet arrived at that state
+            - TRACKING means that the device is moving as commanded
+        """
+        ABORTING = 'aborting'
+        ERROR = 'error'
+        IDLE = 'idle'
+        INITIALIZING = 'initializing'
+        PARKED = 'parked'
+        POSITIONED = 'positioned'
+        SLEWING = 'slewing'
+        TRACKING = 'tracking'
+        UNKNOWN = 'unknown'
 
     def status(self, *args, **kwargs) -> dict:
         """Returns current status of the motion device.

fb8d2b9e879925d65fcc9ed35258bfcba54fd453
Tim-Oliver Husser <mail@husser.de>
2019-03-17 20:07:18 +0100

    re-introduced module GUI, which can primarily be using in a Windows environment, where an interrupt doesn't allow a process to exit gracefully.

---
+++ pyobs/utils/modulegui.py
@@ -0,0 +1,47 @@
+from PyQt5 import QtWidgets, QtGui
+import logging
+
+
+class ModuleGui(QtWidgets.QMainWindow, logging.Handler):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        logging.Handler.__init__(self, *args, **kwargs)
+
+        # basic stuff
+        self.setWindowTitle("pytel")
+        self.resize(900, 500)
+
+        # main menu
+        menu_main = self.menuBar()
+        menu_file = menu_main.addMenu('&File')
+        menu_file.addAction('E&xit', self.close)
+
+        # add QPlainTextEdit
+        self.log = QtWidgets.QTextBrowser(self)
+        self.setCentralWidget(self.log)
+
+        # font
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.log.setFont(font)
+
+        # create log handler
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d %(message)s')
+        logging.root.addHandler(self)
+        self.setFormatter(formatter)
+
+    def emit(self, record):
+        # format entry
+        log_entry = self.format(record)
+
+        # colors?
+        if '[ERROR]' in log_entry:
+            log_entry = '<font color="red">%s</font>' % log_entry
+        elif '[WARNING]' in log_entry:
+            log_entry = '<font color="orange">%s</font>' % log_entry
+
+        # append to log
+        self.log.append(log_entry)
+
+        # scroll to end
+        self.log.verticalScrollBar().setValue(self.log.verticalScrollBar().maximum())

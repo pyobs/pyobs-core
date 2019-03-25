3f20e20455f3bf1bbffa07679cc31139e7a6e518
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-03-25 19:37:00 +0100

    allowing for new filenames on output

--- pyobs/modules/imagedb/newimagewriter.py
+++ pyobs/modules/imagedb/newimagewriter.py
@@ -5,6 +5,7 @@ from queue import Queue
 
 from pyobs import PyObsModule
 from pyobs.events import NewImageEvent
+from pyobs.utils.fits import format_filename
 
 log = logging.getLogger(__name__)
 
@@ -12,17 +13,20 @@ log = logging.getLogger(__name__)
 class NewImageWriter(PyObsModule):
     """Writes new images to disk."""
 
-    def __init__(self, new_images_channel: str = 'new_images', root: str = None, *args, **kwargs):
+    def __init__(self, new_images_channel: str = 'new_images', root: str = None, filenames: str = None,
+                 *args, **kwargs):
         """Creates a new image writer.
 
         Args:
             new_images_channel: Name of new images channel.
             root: VFS root to write files to.
+            filenames: If not None, create new filename from this pattern.
         """
         PyObsModule.__init__(self, thread_funcs=self._worker, *args, **kwargs)
 
         # variables
         self._new_images_channel = new_images_channel
+        self._filenames = filenames
         self._root = root
         self._queue = Queue()
 
@@ -58,22 +62,28 @@ class NewImageWriter(PyObsModule):
                 self.closing.wait(1)
                 continue
             filename = self._queue.get()
-            log.info('Working on file %s...', filename)
 
-            # download image
             try:
-                # output filename
-                output = os.path.join(self._root, os.path.basename(filename))
-
-                # open input/output
-                log.info('Downloading image from %s and storing it as %s...', filename, output)
-                with self.open_file(output, 'wb') as fout:
-                    with self.open_file(filename, 'rb') as fin:
-                        shutil.copyfileobj(fin, fout)
-
+                # download image
+                log.info('Downloading file %s...', filename)
+                hdu = self.vfs.download_fits_image(filename)
             except FileNotFoundError:
                 log.error('Could not download image.')
-                return None
+                continue
+
+            # output filename
+            output = format_filename(hdu, self._filenames, self.environment, filename) if self._filenames else filename
+
+            # add path to output filename
+            output = os.path.join(self._root, os.path.basename(output))
+
+            try:
+                # open output
+                log.info('Storing image as %s...',  output)
+                with self.open_file(output, 'wb') as fout:
+                    hdu.writeto(fout)
+            except Exception:
+                log.error('Could not store image.')
 
 
 __all__ = ['NewImageWriter']

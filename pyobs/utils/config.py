8d2b862c88d331bb0674b4df2ff2bbbcfc7057f3
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-04-03 19:40:59 +0200

    moved pre_process_yaml into separate file

---
+++ pyobs/utils/config.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+
+def pre_process_yaml(config):
+    # read config
+    with open(config, 'r') as f:
+        content = f.read()
+
+    # get path of config
+    path = os.path.dirname(os.path.abspath(config))
+
+    # find all include statements
+    matches = re.findall('(\{include (.*)\})', content)
+    for match, filename in matches:
+        content = content.replace(match, pre_process_yaml(os.path.join(path, filename)))
+
+    # return new yaml
+    return content

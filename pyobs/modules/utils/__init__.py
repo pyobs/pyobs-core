a4529ec82681e0c99544afdefb57c27d906fef82
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-04-16 21:13:52 +0200

    new scriptrunner

--- pyobs/modules/utils/__init__.py
+++ pyobs/modules/utils/__init__.py
@@ -1,1 +1,2 @@
 from .config import Config
+from .scriptrunner import ScriptRunner
---
+++ pyobs/modules/utils/scriptrunner.py
@@ -0,0 +1,37 @@
+import logging
+
+from pyobs.interfaces import IScriptRunner
+from pyobs import PyObsModule
+from pyobs.modules import timeout
+
+
+log = logging.getLogger(__name__)
+
+
+class ScriptRunner(PyObsModule, IScriptRunner):
+    """Config provider."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new script runner."""
+        PyObsModule.__init__(self, *args, **kwargs)
+
+    @timeout(600000)
+    def run_script(self, script: str, *args, **kwargs):
+        """Run the given script.
+
+        Args:
+            script: Script to run.
+
+        Raises:
+            Exception: If anything goes wrong while running the script.
+        """
+
+        # get all proxies
+        proxies = {p: self.comm[p] for p in self.comm.clients}
+
+        # execute it
+        print(script)
+        #exec(script, proxies)
+
+
+__all__ = ['ScriptRunner']

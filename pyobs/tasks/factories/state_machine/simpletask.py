35efbbf85d94e3cd15129b5edc0fc4ff45e50e95
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-06-19 20:27:46 +0200

    finished task

--- pyobs/tasks/factories/state_machine/simpletask.py
+++ pyobs/tasks/factories/state_machine/simpletask.py
@@ -1,7 +1,7 @@
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 
-from pyobs.interfaces import ITelescope, IFocuser, ICamera
+from pyobs.interfaces import ITelescope, IFocuser, ICamera, IFilters
 from pyobs.utils.threads import Future
 from pyobs.utils.time import Time
 from .task import StateMachineTask
@@ -10,15 +10,17 @@ from .task import StateMachineTask
 class SimpleStateMachineTask(StateMachineTask):
     """A simple task for the state machine."""
 
-    def __init__(self, ra=None, dec=None, steps=None, telescope=None, camera=None, *args, **kwargs):
+    def __init__(self, ra: float = None, dec: float = None, steps: list = None, telescope: str = None,
+                 camera: str = None, filters: str = None, *args, **kwargs):
         """Initializes a new Task.
 
         Args:
             ra: RA of target.
             dec: Dec of target.
             steps: List of dictionaries describing step, must contain "filter" and "exptime" and "count".
-            telescope:
-            camera:
+            telescope: Name of ITelescope module to use.
+            camera: Name of ICamera module to use.
+            filters: Name of IFilters module to use.
         """
         StateMachineTask.__init__(self, *args, **kwargs)
 
@@ -32,6 +34,8 @@ class SimpleStateMachineTask(StateMachineTask):
         self._telescope = None
         self._camera_name = camera
         self._camera = None
+        self._filters_name = filters
+        self._filters = None
 
     def __contains__(self, time: Time):
         """Whether the given time is in the interval of this task."""
@@ -41,15 +45,16 @@ class SimpleStateMachineTask(StateMachineTask):
         """Initial steps for a task."""
 
         # get telescope and camera
-        self._telescope = self.comm[self._telescope_name]   # type: (ITelescope, IFocuser)
-        self._camera = self.comm[self._camera_name]         # type: ICamera
+        self._telescope: ITelescope = self.comm[self._telescope_name]
+        self._camera: ICamera = self.comm[self._camera_name]
+        self._filters: IFilters = self.comm[self._filters_name]
 
         # move telescope
         future_track = self._telescope.track(self._coords.ra.degree, self._coords.dec.degree)
 
         # get filter from first step and set it
         self._cur_step = 0
-        future_filter = self._telescope.set_filter(self._steps[self._cur_step]['filter'])
+        future_filter = self._filters.set_filter(self._steps[self._cur_step]['filter'])
 
         # wait for both
         Future.wait_all([future_track, future_filter])
@@ -81,6 +86,7 @@ class SimpleStateMachineTask(StateMachineTask):
         # release proxies
         self._telescope = None
         self._camera = None
+        self._filters = None
 
 
 __all__ = ['SimpleStateMachineTask']

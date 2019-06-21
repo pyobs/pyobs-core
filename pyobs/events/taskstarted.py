bbf17615960bfc99afeea8728fa4c9611f00711c
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-06-21 14:10:32 +0200

    added observation name

--- pyobs/events/taskstarted.py
+++ pyobs/events/taskstarted.py
@@ -2,13 +2,17 @@ from .event import Event
 
 
 class TaskStartedEvent(Event):
-    def __init__(self, name: str = None):
+    def __init__(self, name: str = None, obs: str = None):
         Event.__init__(self)
-        self.data = name
+        self.data = {'name': name, 'obs': obs}
 
     @property
     def name(self):
-        return self.data
+        return self.data['name']
+
+    @property
+    def obs(self):
+        return self.data['obs']
 
 
 __all__ = ['TaskStartedEvent']

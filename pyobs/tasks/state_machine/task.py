b3f0a5976798cdf298fa7950ae5b7100556289d0
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-06-23 18:49:20 +0200

    added automatic calculation of times using sunrise/sunset

--- pyobs/tasks/state_machine/task.py
+++ pyobs/tasks/state_machine/task.py
@@ -1,6 +1,9 @@
 import threading
 from enum import Enum
 from typing import Union
+import astropy.units as u
+from astropy.time import TimeDelta
+
 from pyobs.utils.time import Time
 from pyobs.tasks.task import Task
 
@@ -38,6 +41,21 @@ class StateMachineTask(Task):
         # if it's a Time already, just return it
         if isinstance(t, Time):
             return t
+
+        # if it starts with "sunrise" or "sunset", do some calculations
+        if t.startswith('sunrise') or t.startswith('sunset'):
+            # calculate next sunrise/sunset
+            now = Time.now()
+            time = self.observer.sun_rise_time(now) if t.startswith('sunrise') else self.observer.sun_set_time(now)
+
+            # add or substract secons?
+            if '+' in t:
+                time += TimeDelta(float(t[t.index('+') + 1:]) * u.second)
+            if '-' in t:
+                time -= TimeDelta(float(t[t.index('-') + 1:]) * u.second)
+
+            # finished
+            return time
 
         # if it contains a whitespace or a "T", we assume that it contains a date
         if ' ' in t or 'T' in t:

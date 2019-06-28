adc318700e6bdb8f897e36df3f0b5531b346f95d
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-06-28 13:02:40 +0200

    implementing IFocusModel

--- pyobs/modules/autofocus/focusmodel.py
+++ pyobs/modules/autofocus/focusmodel.py
@@ -2,12 +2,13 @@ import logging
 from py_expression_eval import Parser
 
 from pyobs import PyObsModule
-from pyobs.interfaces import IFocuser, IMotion, IWeather, ITemperatures
+from pyobs.modules import timeout
+from pyobs.interfaces import IFocuser, IMotion, IWeather, ITemperatures, IFocusModel
 
 log = logging.getLogger(__name__)
 
 
-class FocusModel(PyObsModule):
+class FocusModel(PyObsModule, IFocusModel):
     """A focus model that is automatically applied to an IFocuser.
 
     If, e.g., the model is defined as:
@@ -47,6 +48,9 @@ class FocusModel(PyObsModule):
         self._temperatures = temperatures
         self._focuser_ready = True
 
+        # list of allowed focuser states for focussing:
+        self._allowed_states = [IMotion.Status.IDLE, IMotion.Status.POSITIONED,
+                                IMotion.Status.SLEWING, IMotion.Status.TRACKING]
         # model
         parser = Parser()
         self._model = parser.parse(model)
@@ -56,10 +60,6 @@ class FocusModel(PyObsModule):
         # wait a little
         self.closing.wait(1)
 
-        # list of allowed focuser states for focussing:
-        allowed_states = [IMotion.Status.IDLE, IMotion.Status.POSITIONED,
-                          IMotion.Status.SLEWING, IMotion.Status.TRACKING]
-
         # run until closed
         while not self.closing.is_set():
             # get focuser
@@ -67,12 +67,10 @@ class FocusModel(PyObsModule):
                 focuser: IFocuser = self.proxy(self._focuser, IFocuser)
             except ValueError:
                 log.warning('Could not connect to focuser.')
-                self.closing.wait(60)
-                continue
 
             # it must be in allowed state
             status = focuser.get_motion_status('IFocuser').wait()
-            if status not in allowed_states:
+            if status not in self._allowed_states:
                 # log
                 if self._focuser_ready:
                     log.info('Focuser not ready, waiting for it...')
@@ -87,72 +85,105 @@ class FocusModel(PyObsModule):
                 log.info('Focuser ready now, starting focus tracking...')
                 self._focuser_ready = True
 
-            # variables for model evaluation
-            variables = {}
-
-            # do we need a weather proxy?
-            if 'temp' in self._model.variables():
-                log.info('Fetching temperature from weather module...')
-
-                # get weather proxy
-                try:
-                    weather: IWeather = self.proxy(self._weather, IWeather)
-                except ValueError:
-                    log.warning('Could not connect to weather module.')
-                    self.closing.wait(60)
-                    continue
-
-                # get all weather data
-                data = weather.get_weather_status().wait()
-                if IWeather.Sensors.TEMPERATURE.value not in data:
-                    log.warning('No temperature in weather data.')
-                    self.closing.wait(60)
-                    continue
-
-                # get temperature
-                variables['temp'] = data[IWeather.Sensors.TEMPERATURE.value]
-                log.info('Got temperature of %.2f.', variables['temp'])
-
-            # loop other temperatures
-            module_temps = {}
-            for var, cfg in self._temperatures.items():
-                # need to fetch temperatures from module?
-                if cfg['module'] not in module_temps:
-                    log.info('Fetching temperatures from module %s...', cfg['module'])
-
-                    # get proxy
-                    proxy: ITemperatures = self.proxy(cfg['module'], ITemperatures)
-
-                    # get temperatures
-                    module_temps[cfg['module']] = proxy.get_temperatures().wait()
-
-                    # log
-                    vars = ', '.join(['%s=%.2f' % (k, v) for k, v in module_temps[cfg['module']].items()])
-                    log.info('Received temperatures: %s', vars)
-
-                # store, what we need
-                if cfg['sensor'] not in module_temps[cfg['module']]:
-                    log.warning('Temperature for sensor %s not in data from module %s.', cfg['sensor'], cfg['module'])
-                    self.closing.wait(60)
-                    continue
-                variables[var] = module_temps[cfg['module']][cfg['sensor']]
-
-            # log
-            vars = ', '.join(['%s=%.2f' % (k, v) for k, v in variables.items()])
-            log.info('Found values for model: %s', vars)
-
-            # evaluate model
-            log.info('Evaluating model...')
-            focus = self._model.evaluate(variables)
-
-            # set it
-            log.info('Setting optimum focus of %.4f...', focus)
-            focuser.set_focus(focus).wait()
-            log.info('Done.')
+            # now set focus
+            try:
+                # set optimal focus
+                self.set_optimal_focus()
+            except ValueError:
+                # something went wrong, wait a little and continue
+                self.closing.wait(60)
+                continue
 
             # sleep interval
             log.info('Going to sleep for %d seconds...', self._interval)
             self.closing.wait(self._interval)
+
+    def get_optimal_focus(self, *args, **kwargs) -> float:
+        """Returns the optimal focus.
+
+        Returns:
+            Optimum focus calculated from model.
+
+        Raises:
+            ValueError: If anything went wrong.
+        """
+
+        # variables for model evaluation
+        variables = {}
+
+        # do we need a weather proxy?
+        if 'temp' in self._model.variables():
+            log.info('Fetching temperature from weather module...')
+
+            # get weather proxy
+            try:
+                weather: IWeather = self.proxy(self._weather, IWeather)
+            except ValueError:
+                raise ValueError('Could not connect to weather module.')
+
+            # get all weather data
+            data = weather.get_weather_status().wait()
+            if IWeather.Sensors.TEMPERATURE.value not in data:
+                raise ValueError('No temperature in weather data.')
+
+            # get temperature
+            variables['temp'] = data[IWeather.Sensors.TEMPERATURE.value]
+            log.info('Got temperature of %.2f.', variables['temp'])
+
+        # loop other temperatures
+        module_temps = {}
+        for var, cfg in self._temperatures.items():
+            # need to fetch temperatures from module?
+            if cfg['module'] not in module_temps:
+                log.info('Fetching temperatures from module %s...', cfg['module'])
+
+                # get proxy
+                proxy: ITemperatures = self.proxy(cfg['module'], ITemperatures)
+
+                # get temperatures
+                module_temps[cfg['module']] = proxy.get_temperatures().wait()
+
+                # log
+                vars = ', '.join(['%s=%.2f' % (k, v) for k, v in module_temps[cfg['module']].items()])
+                log.info('Received temperatures: %s', vars)
+
+            # store, what we need
+            if cfg['sensor'] not in module_temps[cfg['module']]:
+                raise ValueError('Temperature for sensor %s not in data from module %s.' %
+                                 (cfg['sensor'], cfg['module']))
+            variables[var] = module_temps[cfg['module']][cfg['sensor']]
+
+        # log
+        vars = ', '.join(['%s=%.2f' % (k, v) for k, v in variables.items()])
+        log.info('Found values for model: %s', vars)
+
+        # evaluate model
+        log.info('Evaluating model...')
+        focus = self._model.evaluate(variables)
+        log.info('Found optimal focus of %.4f.', focus)
+        return focus
+
+    @timeout(60000)
+    def set_optimal_focus(self, *args, **kwargs):
+        """Sets optimal focus.
+
+        Raises:
+            ValueError: If anything went wrong.
+        """
+
+        # get focuser
+        try:
+            focuser: IFocuser = self.proxy(self._focuser, IFocuser)
+        except ValueError:
+            log.warning('Could not connect to focuser.')
+
+        # get focus
+        focus = self.get_optimal_focus()
+
+        # set it
+        log.info('Setting optimal focus...')
+        focuser.set_focus(focus).wait()
+        log.info('Done.')
 
 
 __all__ = ['FocusModel']

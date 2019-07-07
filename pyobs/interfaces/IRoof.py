ab4554d40bb9e6d5d14b2901edb2530888bf39a0
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-07-07 18:05:36 +0200

    open/close/stop and reacting to weather

--- pyobs/interfaces/IRoof.py
+++ pyobs/interfaces/IRoof.py
@@ -3,18 +3,7 @@ from .IMotion import IMotion
 
 class IRoof(IMotion):
     """Base interface for all observatory enclosures."""
-
-    def open_roof(self, *args, **kwargs):
-        """Open the roof."""
-        raise NotImplementedError
-
-    def close_roof(self, *args, **kwargs):
-        """Close the roof."""
-        raise NotImplementedError
-
-    def get_percent_open(self) -> float:
-        """Get the percentage the roof is open."""
-        raise NotImplementedError
+    pass
 
 
 __all__ = ['IRoof']

d26a904a3aadb4f5e2829022c6e280cbbd8066d9
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-07-07 13:14:27 +0200

    added units

--- pyobs/interfaces/IWeather.py
+++ pyobs/interfaces/IWeather.py
@@ -5,16 +5,16 @@ from .interface import Interface
 
 class IWeather(Interface):
     class Sensors(Enum):
-        TIME = 'time'
-        TEMPERATURE = 'temp'
-        HUMIDITY = 'humid'
-        PRESSURE = 'press'
-        WINDDIR = 'winddir'
-        WINDSPEED = 'windspeed'
-        RAIN = 'rain'
-        SKYTEMP = 'skytemp'
-        DEWPOINT = 'dewpoint'
-        PARTICLES = 'particles'
+        TIME = 'time'               # in iso format
+        TEMPERATURE = 'temp'        # in °C
+        HUMIDITY = 'humid'          # in %
+        PRESSURE = 'press'          # in hPa
+        WINDDIR = 'winddir'         # in degrees azimuth
+        WINDSPEED = 'windspeed'     # in km/h
+        RAIN = 'rain'               # 0/1
+        SKYTEMP = 'skytemp'         # in °C
+        DEWPOINT = 'dewpoint'       # in °C
+        PARTICLES = 'particles'     # in particles per m³
 
     def get_weather_status(self, *args, **kwargs) -> dict:
         """Returns status of object in form of a dictionary. See other interfaces for details."""

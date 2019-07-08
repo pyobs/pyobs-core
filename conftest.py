1e6fb814bd9020a4c7617ff4f903f1feb562dd9a
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-07-08 16:25:02 +0200

    - updated handling of --use-ssh switch - added session fixture for downloading IERS data

--- conftest.py
+++ conftest.py
@@ -1,5 +1,26 @@
 import pytest
+import astroplan
 
 
 def pytest_addoption(parser):
     parser.addoption("--use-ssh", action="store_true", help="do SSH tests")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "ssh: mark test as using SSH")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--use-ssh"):
+        # do not skip slow tests
+        return
+    nossh = pytest.mark.skip(reason="SSH testing disabled (use --use-ssh to activate.")
+    for item in items:
+        if "ssh" in item.keywords:
+            item.add_marker(nossh)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def download_IERS():
+    # before starting any test, download the new IERS data
+    astroplan.download_IERS_A()

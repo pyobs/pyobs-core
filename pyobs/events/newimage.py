9613398bdf40d6c09925f84a46794876b66e6c46
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-07-25 20:33:25 -0500

    added image_type to event

--- pyobs/events/newimage.py
+++ pyobs/events/newimage.py
@@ -1,16 +1,23 @@
 from .event import Event
 
+from pyobs.interfaces import ICamera
+
 
 class NewImageEvent(Event):
-    def __init__(self, filename=None):
+    def __init__(self, filename: str = None, image_type: ICamera.ImageType = None):
         Event.__init__(self)
         self.data = {
-            'filename': filename
+            'filename': filename,
+            'image_type': image_type.value
         }
 
     @property
     def filename(self):
         return self.data['filename']
+
+    @property
+    def image_type(self):
+        return ICamera.ImageType(self.data['image_type'])
 
 
 __all__ = ['NewImageEvent']

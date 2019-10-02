6b2f35a6c783502644a101ad881c19a5433031d6
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-10-02 21:40:46 +0200

    added __init__

--- pyobs/utils/astrometry/astrometry.py
+++ pyobs/utils/astrometry/astrometry.py
@@ -2,6 +2,9 @@ from pyobs.utils.images import Image
 
 
 class Astrometry:
+    def __init__(self, *args, **kwargs):
+        pass
+
     def __call__(self, image: Image):
         raise NotImplementedError
 

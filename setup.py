aea99aece9c2f8e7225498580a9519bfdf3ec985
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-11-17 15:11:19 +0100

    just for PyPi...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyobs-core',
-    version='0.8.1',
+    version='0.8.2',
     description='robotic telescope software',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',

6c42839324d967e379fbb6d1272b1673766a9b6b
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-11-26 18:08:59 +0100

    added interface attribute

--- pyobs/events/motionstatuschanged.py
+++ pyobs/events/motionstatuschanged.py
@@ -3,11 +3,11 @@ from .event import Event
 
 
 class MotionStatusChangedEvent(Event):
-    def __init__(self, last: IMotion.Status = None, current: IMotion.Status = None):
+    def __init__(self, last: IMotion.Status = None, current: IMotion.Status = None, interface: str = None):
         Event.__init__(self)
         self.data = None
         if last is not None and current is not None:
-            self.data = {'last': last.value, 'current': current.value}
+            self.data = {'last': last.value, 'current': current.value, 'interface': interface}
 
     @property
     def last(self):
@@ -16,6 +16,10 @@ class MotionStatusChangedEvent(Event):
     @property
     def current(self):
         return None if self.data is None else IMotion.Status(self.data['current'])
+
+    @property
+    def interface(self):
+        return self.data['interface']
 
 
 __all__ = ['MotionStatusChangedEvent']

1b1aa08b53832a1eb5361bdfdeb149d6612d7f7d
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-12-09 21:35:02 +0100

    added file

---
+++ pyobs/utils/curvefit.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+def fit_hyperbola(x_arr, y_arr, y_err):
+    """Fit a hyperbola
+
+    Args:
+        x_arr: X data
+        y_arr: Y data
+        y_err: Y errors
+
+    Returns:
+        Minimum of hyperbola and its uncertainty
+    """
+
+    # initial guess
+    ic = np.argmin(y_arr)
+    ix = np.argmax(y_arr)
+    b = y_arr[ic]
+    c = x_arr[ic]
+    x = x_arr[ix]
+    slope = np.abs((y_arr[ic] - y_arr[ix]) / (c - x))
+    a = b / slope
+
+    # init
+    p0 = [a, b, c]
+
+    # fit
+    coeffs, cov = curve_fit(lambda xx, aa, bb, cc: bb * np.sqrt((xx - cc) ** 2 / aa ** 2 + 1.),
+                            x_arr, y_arr, sigma=y_err, p0=p0)
+
+    # return result
+    return coeffs[2], cov[2][2]

dbe987dc297ca111ca0d9ac7a639bb59bc21590f
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-02-14 18:19:35 +0200

    added "id" to event

--- pyobs/events/taskstarted.py
+++ pyobs/events/taskstarted.py
@@ -1,24 +1,33 @@
+import typing
+
 from pyobs.utils.time import Time
 from .event import Event
 
 
 class TaskStartedEvent(Event):
-    def __init__(self, name: str = None, eta: Time = None):
+    def __init__(self, name: str = None, id: typing.Any = None, eta: Time = None):
         """Initializes a new task started event.
 
         Args:
             name: Name of task that just started
+            id: Unique identifier for task
             eta: Predicted ETA for when the task will finish
+
         """
         Event.__init__(self)
         self.data = {
             'name': name,
+            'id': id,
             'eta':  None if eta is None else eta.isot
         }
 
     @property
     def name(self):
         return self.data['name']
+
+    @property
+    def id(self):
+        return self.data['id']
 
     @property
     def eta(self):

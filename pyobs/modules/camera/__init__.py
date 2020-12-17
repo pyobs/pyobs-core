52cc500676e3bdd3cbfbed8eacf3d9d04591ebc5
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-12-17 11:32:24 +0100

    removed import for AdaptiveCamera

--- pyobs/modules/camera/__init__.py
+++ pyobs/modules/camera/__init__.py
@@ -1,4 +1,4 @@
 from .basecamera import BaseCamera
-from .adaptive import AdaptiveCamera
+#from .adaptive import AdaptiveCamera
 from .dummycamera import DummyCamera
 from .flatfieldtestcamera import FlatFieldTestCamera

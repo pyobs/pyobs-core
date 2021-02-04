b922a596a1f72408e0979fea224047fb0900e4c4
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-02-04 17:39:36 +0100

    added import

--- pyobs/modules/utils/__init__.py
+++ pyobs/modules/utils/__init__.py
@@ -1,5 +1,6 @@
 from .autonomouswarning import AutonomousWarning
 from .jobscheduler import JobScheduler
+from .kiosk import Kiosk
 from .scriptrunner import ScriptRunner
 from .telegram import Telegram
 from .trigger import Trigger

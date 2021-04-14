b82f570530ceb5a25ac5d536e39e82112c200b14
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-04-14 17:37:16 +0200

    fixed docstring

--- pyobs/interfaces/IAutoFocus.py
+++ pyobs/interfaces/IAutoFocus.py
@@ -11,7 +11,7 @@ class IAutoFocus(IAbortable):
 
         This method performs an auto-focus series with "count" images on each side of the initial guess and the given
         step size. With count=3, step=1 and guess=10, this takes images at the following focus values:
-            7, 8, 9, 10, 11, 12, 13
+        7, 8, 9, 10, 11, 12, 13
 
         Args:
             count: Number of images to take on each side of the initial guess. Should be an odd number.

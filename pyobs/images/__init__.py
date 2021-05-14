870142226ac3bedb430d5d688d11a5bc8d79d5d3
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-05-14 09:26:35 +0200

    removed classes for calibration images

--- pyobs/images/__init__.py
+++ pyobs/images/__init__.py
@@ -4,8 +4,4 @@ Some info about :class:`pyobs.images.Image`.
 __title__ = 'Images'
 
 from .image import Image
-from .calibration import CalibrationImage
-from .bias import BiasImage
-from .dark import DarkImage
-from .flat import FlatImage
 from .processor import ImageProcessor

ba5b150d3aa259a5b958b712e0fca54606e11bc4
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-06-20 15:05:04 +0200

    adjusted video serving

--- pyobs/modules/camera/basewebcam.py
+++ pyobs/modules/camera/basewebcam.py
@@ -22,7 +22,7 @@ INDEX_HTML = """
     <title>Title</title>
   </head>
   <body>
-    <img src="/video">
+    <img src="/video.mjpg">
   </body>
 </html>
 
@@ -43,13 +43,14 @@ class VideoHandler(tornado.web.RequestHandler):
         self.set_header('Connection', 'close')
 
         self.served_image_timestamp = time.time()
-        my_boundary = "--jpgboundary"
+        my_boundary = "--jpgboundary\r\n"
+        last_num = None
         while True:
             try:
                 # Generating images for mjpeg stream and wraps them into http resp
-                interval = 0.1
-                if self.served_image_timestamp + interval < time.time():
-                    image = self.application.image_jpeg
+                num, image = self.application.image_jpeg
+                if num != last_num:
+                    last_num = num
                     self.write(my_boundary)
                     self.write("Content-type: image/jpeg\r\n")
                     self.write("Content-length: %s\r\n\r\n" % len(image))
@@ -57,7 +58,7 @@ class VideoHandler(tornado.web.RequestHandler):
                     self.served_image_timestamp = time.time()
                     yield self.flush()
                 else:
-                    yield tornado.gen.sleep(interval)
+                    yield tornado.gen.sleep(0.1)
             except tornado.iostream.StreamClosedError:
                 # stream closed
                 break
@@ -69,7 +70,7 @@ class ImageHandler(tornado.web.RequestHandler):
         self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0')
         self.set_header('Pragma', 'no-cache')
         self.set_header('Content-Type', 'image/fits')
-        image = self.application.image_fits
+        _, image = self.application.image_fits
         self.set_header("Content-length", len(image))
         self.write(image)
 
@@ -79,7 +80,7 @@ class BaseWebcam(Module, tornado.web.Application, IWebcam, ICameraExposureTime):
     __module__ = 'pyobs.modules.camera'
 
     def __init__(self, http_port: int = 37077, interval: float = 0.5, image_path: str = '/webcam/image.fits',
-                 video_path: str = '/webcam/video', *args, **kwargs):
+                 video_path: str = '/webcam/video.mjpg', *args, **kwargs):
         """Creates a new BaseWebcam.
 
         On the receiving end, a VFS root with a HTTPFile must exist with the same name as in image_path and video_path,
@@ -103,11 +104,12 @@ class BaseWebcam(Module, tornado.web.Application, IWebcam, ICameraExposureTime):
         self._new_image_event = threading.Event()
         self._image_path = image_path
         self._video_path = video_path
+        self._frame_num = 0
 
         # init tornado web server
         tornado.web.Application.__init__(self, [
             (r"/", MainHandler),
-            (r"/video", VideoHandler),
+            (r"/video.mjpg", VideoHandler),
             (r"/image.fits", ImageHandler),
         ])
         self._image_fits = None
@@ -152,12 +154,12 @@ class BaseWebcam(Module, tornado.web.Application, IWebcam, ICameraExposureTime):
     @property
     def image_fits(self):
         with self._lock:
-            return self._image_fits
+            return self._frame_num, self._image_fits
 
     @property
     def image_jpeg(self):
         with self._lock:
-            return self._image_jpeg
+            return self._frame_num, self._image_jpeg
 
     def _set_image(self, data: np.ndarray):
         """Create FITS and JPEG images from data."""
@@ -181,6 +183,7 @@ class BaseWebcam(Module, tornado.web.Application, IWebcam, ICameraExposureTime):
             self._image_time = now
             self._image_fits = image_fits
             self._image_jpeg = image_jpeg
+            self._frame_num += 1
 
         # signal it
         self._new_image_event.set()

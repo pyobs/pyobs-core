b9fcef00af4b7ccbdb4821ad07e17943e2a5ea82
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-09-24 10:14:06 +0200

    added Offsets.on_sky_distance and use it in Acquisition

--- pyobs/images/processors/offsets/offsets.py
+++ pyobs/images/processors/offsets/offsets.py
@@ -1,4 +1,5 @@
 import logging
+from astropy.wcs import WCS
 
 from pyobs.images import Image
 from pyobs.images.processor import ImageProcessor
@@ -24,6 +25,31 @@ class Offsets(ImageProcessor):
             ValueError: If offset could not be found.
         """
         raise NotImplementedError
+
+    @staticmethod
+    def on_sky_distance(image: Image) -> float:
+        """Calculate on sky distance for offset calculated by any of the derived classes in degrees.
+
+        Args:
+            image: Image to analyse. Needs CRPIX1/2 entries in header and 'offsets' in meta.
+
+        Returns:
+            On-sky offset of offset in degrees.
+        """
+
+        # get central position and offset
+        center = image.header['CRPIX1'], image.header['CRPIX2']
+        offsets = image.meta['offsets']
+
+        # get WCS
+        wcs = WCS(image.header)
+
+        # get RA/Dec at center and at center+offsets
+        center_coord = wcs.pixel_to_world(*center)
+        offset_coord = wcs.pixel_to_world(*tuple(map(sum, zip(center, offsets))))
+
+        # calculate distance and return it
+        return center_coord.separation(offset_coord)
 
 
 __all__ = ['Offsets']
--- pyobs/modules/acquisition/acquisition.py
+++ pyobs/modules/acquisition/acquisition.py
@@ -5,12 +5,14 @@ from astropy.coordinates import SkyCoord, AltAz
 from astropy.wcs import WCS
 import astropy.units as u
 
+from pyobs.images.processors.offsets import Offsets
 from pyobs.interfaces import ITelescope, ICamera, IAcquisition, IRaDecOffsets, IAltAzOffsets, IExposureTime, \
     IImageType
 from pyobs.mixins.pipeline import PipelineMixin
 from pyobs.modules import Module
 from pyobs.mixins import CameraSettingsMixin
 from pyobs.modules import timeout
+from pyobs.object import get_object
 from pyobs.utils.enums import ImageType
 from pyobs.images import Image, ImageProcessor
 from pyobs.images.processors.misc import SoftBin
@@ -28,7 +30,7 @@ class Acquisition(Module, CameraSettingsMixin, IAcquisition, PipelineMixin):
     def __init__(self, telescope: Union[str, ITelescope], camera: Union[str, ICamera],
                  offsets: List[Union[dict, ImageProcessor]], apply: Union[dict, ApplyOffsets],
                  target_pixel: Tuple = None, attempts: int = 5, tolerance: float = 1,
-                 max_offset: float = 120, log_file: str = None, soft_bin: int = None, *args, **kwargs):
+                 max_offset: float = 120, log_file: str = None, *args, **kwargs):
         """Create a new acquisition.
 
         Args:
@@ -59,8 +61,8 @@ class Acquisition(Module, CameraSettingsMixin, IAcquisition, PipelineMixin):
         # init log file
         self._publisher = CsvPublisher(log_file)
 
-        # binning
-        self._soft_bin = None if soft_bin is None else SoftBin(binning=soft_bin)
+        # apply offsets
+        self._apply = get_object(apply, ApplyOffsets)
 
         # init camera settings mixin
         CameraSettingsMixin.__init__(self, *args, **kwargs)
@@ -118,59 +120,13 @@ class Acquisition(Module, CameraSettingsMixin, IAcquisition, PipelineMixin):
 
             # download image
             log.info('Downloading image...')
-            img = self.vfs.read_image(filename)
+            image = self.vfs.read_image(filename)
 
             # get offset
             image = self.run_pipeline(image)
 
-            # bin?
-            if self._soft_bin is not None:
-                self._soft_bin(img)
-
-            # get target pixel
-            if self._target_pixel is None:
-                cy, cx = (np.array(img.data.shape) / 2.).astype(np.int)
-            else:
-                cx, cy = self._target_pixel
-
-            # get date obs and wcc
-            date_obs = Time(img.header['DATE-OBS'])
-            wcs = WCS(img.header)
-
-            # coordinates without offset
-            ra, dec = img.header['CRVAL1'], img.header['CRVAL2']
-
-            # get WCS and RA/DEC for target pixel and
-            lon, lat = wcs.all_pix2world(cx, cy, 0)
-            radec_center = SkyCoord(ra=lon * u.deg, dec=lat * u.deg, frame='icrs', obstime=date_obs,
-                                    location=self.location)
-
-            # get required shift in RA/Dec
-            log.info('Analyzing image...')
-            x_target, y_target = self._get_target_radec(img, ra, dec)
-
-            # convert to world
-            ra_target, dec_target = wcs.all_pix2world(x_target, y_target, 0)
-            radec_target = SkyCoord(ra=ra_target * u.deg, dec=dec_target * u.deg, frame='icrs',
-                                    obstime=date_obs, location=self.location)
-
-            # get current position (without offsets!)
-            cur_ra, cur_dec = telescope.get_radec().wait()
-            radec_current = SkyCoord(ra=cur_ra * u.deg, dec=cur_dec * u.deg, frame='icrs',
-                                     obstime=date_obs, location=self.location)
-
-            # calculate offsets and return them
-            dra = (radec_target.ra.degree - radec_center.ra.degree) * np.cos(np.radians(cur_dec))
-            ddec = radec_target.dec.degree - radec_center.dec.degree
-            dist = radec_center.separation(radec_target).degree
-            log.info('Found RA/Dec shift of dRA=%.2f", dDec=%.2f", giving %.2f" in total.',
-                     dra * 3600., ddec * 3600., dist * 3600.)
-
-            # for testing, calculate offsets from current
-            t1, t2 = radec_current.spherical_offsets_to(radec_target)
-            log.info('TESTING current->target: dRA=%.2f", dDec=%.2f"', t1.arcsec, t2.arcsec)
-            t1, t2 = radec_center.spherical_offsets_to(radec_target)
-            log.info('TESTING center->target: dRA=%.2f", dDec=%.2f"', t1.arcsec, t2.arcsec)
+            # calculate distance from offset
+            dist = Offsets.on_sky_distance(image)
 
             # get distance
             if dist * 3600. < self._tolerance:
@@ -179,6 +135,7 @@ class Acquisition(Module, CameraSettingsMixin, IAcquisition, PipelineMixin):
 
                 # get current Alt/Az
                 cur_alt, cur_az = telescope.get_altaz().wait()
+                cur_ra, cur_dec = telescope.get_radec().wait()
 
                 # prepare log entry
                 log_entry = {
@@ -207,57 +164,21 @@ class Acquisition(Module, CameraSettingsMixin, IAcquisition, PipelineMixin):
                 # move a maximum of 120"=2'
                 raise ValueError('Calculated offsets too large.')
 
-            # is telescope on an equitorial mount?
-            if isinstance(telescope, IRaDecOffsets):
-                # get current offset
-                cur_dra, cur_ddec = telescope.get_radec_offsets().wait()
+            # get telescope
+            try:
+                telescope: ITelescope = self.proxy(self._telescope, ITelescope)
+            except ValueError:
+                log.error('Given telescope does not exist or is not of correct type.')
+                continue
 
-                # calculate total offsets
-                total_dra, total_ddec = float(cur_dra + dra), float(cur_ddec + ddec)
-
-                # move offset
-                log.info('Offsetting telescope to dRA=%.2f", dDec=%.2f"...', total_dra * 3600., total_ddec * 3600.)
-                telescope.set_radec_offsets(total_dra, total_ddec).wait()
-
-            elif isinstance(telescope, IAltAzOffsets):
-                # transform both to Alt/AZ
-                altaz1 = radec_center.transform_to(AltAz)
-                altaz2 = radec_target.transform_to(AltAz)
-
-                # calculate offsets
-                dalt = altaz2.alt.degree - altaz1.alt.degree
-                daz = altaz2.az.degree - altaz1.az.degree
-                log.info('Transformed to Alt/Az shift of dalt=%.2f", daz=%.2f".', dalt * 3600., daz * 3600.)
-
-                # get current offset
-                cur_dalt, cur_daz = telescope.get_altaz_offsets().wait()
-                log.info('Current offsets alt=%.2f, az=%.2f.', cur_dalt * 3600, cur_daz * 3600)
-
-                # move offset
-                log.info('Offsetting telescope...')
-                telescope.set_altaz_offsets(float(cur_dalt + dalt), float(cur_daz + daz)).wait()
-
+            # apply offsets
+            if self._apply(image, telescope, self.location):
+                log.info('Finished image.')
             else:
-                log.warning('Telescope has neither altaz nor equitorial mount. No idea how to move it...')
+                log.warning('Could not apply offsets.')
 
         # could not acquire target
         raise ValueError('Could not acquire target within given tolerance.')
 
-    def _get_target_radec(self, img: Image, ra: float, dec: float) -> Tuple[float, float]:
-        """Returns RA/Dec coordinates of pixel that needs to be centered.
 
-        Params:
-            img: Image to analyze.
-            ra: Requested RA.
-            dec: Requested Declination.
-
-        Returns:
-            (ra, dec) of pixel that needs to be moved to the centre of the image.
-
-        Raises:
-            ValueError: If target coordinates could not be determined.
-        """
-        raise NotImplemented
-
-
-__all__ = ['BaseAcquisition']
+__all__ = ['Acquisition']

3aa67e6737c056e6c25c87244866e10c8122200e
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-10-10 10:52:01 +0200

    started working on new Broadcast image processor

---
+++ pyobs/images/processors/misc/broadcast.py
@@ -0,0 +1,39 @@
+import logging
+
+from pyobs.images.processor import ImageProcessor
+from pyobs.images import Image
+
+
+log = logging.getLogger(__name__)
+
+
+class Broadcast(ImageProcessor):
+    """Broadcast image."""
+    __module__ = 'pyobs.images.processors.misc'
+
+    def __init__(self, copy: bool = True, filename: str = '/cache/{FNAME}', *args, **kwargs):
+        """Init an image processor that broadcasts an image
+
+        Args:
+            copy: If True, copy image to given filename before broadcasting.
+            filename: New filename, only used if copy=True.
+        """
+
+        # store
+        self._copy = copy
+        self._filename = filename
+
+    def __call__(self, image: Image) -> Image:
+        """Broadcast image.
+
+        Args:
+            image: Image to broadcast.
+
+        Returns:
+            Original image.
+        """
+        pass
+
+
+__all__ = ['Broadcast']
+

698032d41c09f123b2aae51b91e3079e280e5d2a
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-10-18 10:26:33 +0200

    removed exposure_time parameter

--- pyobs/interfaces/IAcquisition.py
+++ pyobs/interfaces/IAcquisition.py
@@ -5,14 +5,11 @@ class IAcquisition(IRunning):
     """The module can acquire a target, usually by accessing a telescope and a camera."""
     __module__ = 'pyobs.interfaces'
 
-    def acquire_target(self, exposure_time: float, *args, **kwargs) -> dict:
+    def acquire_target(self, *args, **kwargs) -> dict:
         """Acquire target at given coordinates.
 
         If no RA/Dec are given, start from current position. Might not work for some implementations that require
         coordinates.
-
-        Args:
-            exposure_time: Exposure time for acquisition in seconds.
 
         Returns:
             A dictionary with entries for datetime, ra, dec, alt, az, and either off_ra, off_dec or off_alt, off_az.

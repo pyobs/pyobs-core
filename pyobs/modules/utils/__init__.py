5dfd64609ab65d412a94ea8559ebb95f8665f91f
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-10-25 16:55:31 +0200

    removed ScriptRunner

--- pyobs/modules/utils/__init__.py
+++ pyobs/modules/utils/__init__.py
@@ -8,6 +8,5 @@ from .autonomouswarning import AutonomousWarning
 from .fluentlogger import FluentLogger
 from .httpfilecache import HttpFileCache
 from .kiosk import Kiosk
-from .scriptrunner import ScriptRunner
 from .telegram import Telegram
 from .trigger import Trigger

d1ffa60f7a78a1e57e2de1cf3f0a62c9ff9837ff
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-12-06 13:39:03 +0100

    added method

---
+++ pyobs/utils/parallel.py
@@ -0,0 +1,9 @@
+import asyncio
+import contextlib
+
+
+async def event_wait(evt, timeout):
+    # suppress TimeoutError because we'll return False in case of timeout
+    with contextlib.suppress(asyncio.TimeoutError):
+        await asyncio.wait_for(evt.wait(), timeout)
+    return evt.is_set()

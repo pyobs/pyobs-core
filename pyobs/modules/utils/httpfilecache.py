2fbfdf26074f9aeb7b7b679950d9c1fc6d04196a
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-12-20 16:46:00 +0100

    open file cache for 0.0.0.0

--- pyobs/modules/utils/httpfilecache.py
+++ pyobs/modules/utils/httpfilecache.py
@@ -42,7 +42,7 @@ class HttpFileCache(Module):
         # start listening
         log.info('Starting HTTP file cache on port %d...', self._port)
         await self._runner.setup()
-        self._site = web.TCPSite(self._runner, 'localhost', self._port)
+        self._site = web.TCPSite(self._runner, '0.0.0.0', self._port)
         await self._site.start()
         self._is_listening = True
 

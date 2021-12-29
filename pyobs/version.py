44613d77020e9a1cb59ad53a0b696cfb10816afd
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-12-29 18:52:17 +0100

    added version method

--- pyobs/version.py
+++ pyobs/version.py
@@ -2,11 +2,15 @@ import importlib.metadata
 from typing import Tuple
 
 
-__version__ = importlib.metadata.version('pyobs-core')
+def version() -> str:
+    return importlib.metadata.version('pyobs-core')
 
 
-def version_tuple() -> Tuple[int, ...]:
-    return tuple(map(int, (__version__.split("."))))
+__version__ = version()
 
 
-__all__ = ['__version__', 'version_tuple']
+def version_tuple(v: str = __version__) -> Tuple[int, ...]:
+    return tuple(map(int, (v.split("."))))
+
+
+__all__ = ['version', '__version__', 'version_tuple']

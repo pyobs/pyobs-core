1e20ca0a7e28cecbdf0ee38dd119d948615d489f
Tim-Oliver Husser <thusser@uni-goettingen.de>
2022-01-01 20:13:21 +0100

    current() cannot be None

--- pyobs/events/exposurestatuschanged.py
+++ pyobs/events/exposurestatuschanged.py
@@ -5,42 +5,43 @@ from pyobs.events.event import Event
 from pyobs.utils.enums import ExposureStatus
 
 
-DataType = TypedDict('DataType', {'last': Optional[str], 'current': str})
+DataType = TypedDict("DataType", {"last": Optional[str], "current": str})
 
 
 class ExposureStatusChangedEvent(Event):
     """Event to be sent, when the exposure status of a device changes."""
-    __module__ = 'pyobs.events'
 
-    def __init__(self, current: ExposureStatus,  last: Optional[ExposureStatus] = None, **kwargs: Any):
+    __module__ = "pyobs.events"
+
+    def __init__(self, current: ExposureStatus, last: Optional[ExposureStatus] = None, **kwargs: Any):
         Event.__init__(self)
         self.data: DataType = {
-            'last': last.value if last is not None else None,
-            'current': current.value
+            "last": last.value if last is not None else None,
+            "current": current.value,
         }
 
     @classmethod
     def from_dict(cls, d: Dict[str, Any]) -> Event:
         # get current
-        if 'current' not in d or not isinstance(d['current'], str):
-            raise ValueError('Invalid type for current.')
-        current: ExposureStatus = ExposureStatus(d['current'])
+        if "current" not in d or not isinstance(d["current"], str):
+            raise ValueError("Invalid type for current.")
+        current: ExposureStatus = ExposureStatus(d["current"])
 
         # get last
         last: Optional[ExposureStatus] = None
-        if 'last' in d and isinstance(d['last'], str):
-            last = ExposureStatus(d['last'])
+        if "last" in d and isinstance(d["last"], str):
+            last = ExposureStatus(d["last"])
 
         # return object
         return ExposureStatusChangedEvent(current=current, last=last)
 
     @property
     def last(self) -> Optional[ExposureStatus]:
-        return ExposureStatus(self.data['last']) if self.data['last'] is not None else None
+        return ExposureStatus(self.data["last"]) if self.data["last"] is not None else None
 
     @property
-    def current(self) -> Optional[ExposureStatus]:
-        return ExposureStatus(self.data['current']) if self.data['current'] is not None else None
+    def current(self) -> ExposureStatus:
+        return ExposureStatus(self.data["current"])
 
 
-__all__ = ['ExposureStatusChangedEvent']
+__all__ = ["ExposureStatusChangedEvent"]

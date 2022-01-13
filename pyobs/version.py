e22ac031846a1d021799e1f9783be80ffa37c842
Tim-Oliver Husser <thusser@uni-goettingen.de>
2022-01-13 14:51:18 +0100

    explicitly use pyobs-core as package name

--- pyobs/version.py
+++ pyobs/version.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from single_source import get_version
 
 
-__version__ = get_version(__name__, Path(__file__).parent.parent)
+__version__ = get_version("pyobs-core", Path(__file__).parent.parent)
 
 
 def version() -> str:

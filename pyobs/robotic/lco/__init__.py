1f779f4c01b2deb3515037316713283f29f1327c
Tim-Oliver Husser <thusser@uni-goettingen.de>
2022-02-01 15:06:40 +0100

    added dummy task schedule

--- pyobs/robotic/lco/__init__.py
+++ pyobs/robotic/lco/__init__.py
@@ -2,3 +2,4 @@ from .task import LcoTask
 from .taskarchive import LcoTaskArchive
 from .taskschedule import LcoTaskSchedule
 from .taskrunner import LcoTaskRunner
+from .dummytaskschedule import LcoDummyTaskSchedule

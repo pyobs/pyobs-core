eb55963c43b75462aaed9a56a3d7c0df12e130c6
Tim-Oliver Husser <thusser@uni-goettingen.de>
2022-02-28 16:11:17 +0100

    removed default values for cli parameters from environment

--- pyobs/cli/pyobs.py
+++ pyobs/cli/pyobs.py
@@ -10,23 +10,14 @@ def init_cli():
     parser = argparse.ArgumentParser()
 
     # config
-    parser.add_argument("config", type=str, help="Configuration file", nargs="?", default=os.environ.get("CONFIG"))
+    parser.add_argument("config", type=str, help="Configuration file")
 
     # logging
-    parser.add_argument(
-        "--log-level",
-        type=str,
-        choices=["critical", "error", "warning", "info", "debug"],
-        default=os.environ.get("PYOBS_LOG_LEVEL", "info"),
-    )
-    parser.add_argument(
-        "-l", "--log-file", type=str, help="file to write log into", default=os.environ.get("PYOBS_LOG_FILE")
-    )
+    parser.add_argument("--log-level", type=str, choices=["critical", "error", "warning", "info", "debug"])
+    parser.add_argument("-l", "--log-file", type=str, help="file to write log into")
 
     # debug stuff
-    parser.add_argument(
-        "--debug-time", type=str, help="Fake time at start for pyobs to use", default=os.environ.get("PYOBS_DEBUG_TIME")
-    )
+    parser.add_argument("--debug-time", type=str, help="Fake time at start for pyobs to use")
 
     # return it
     return parser
@@ -89,7 +80,7 @@ def main() -> None:
 
     # init argument parsing and add PID/Daemon stuff
     parser = init_cli()
-    parser.add_argument("-p", "--pid-file", type=str, default=os.environ.get("PIDFILE"))
+    parser.add_argument("-p", "--pid-file", type=str)
 
     # parse it
     args = parse_cli(parser)

76bc4fa7bb30246544b979aa16d2ed52c3bb3be7
GermanHydrogen <admin@germanhydrogen.org>
2023-08-29 18:58:08 +0200

    Implementented guiding stat calculator

--- pyobs/utils/guiding_stat/guiding_stat_calculator.py
+++ pyobs/utils/guiding_stat/guiding_stat_calculator.py
@@ -1,3 +1,8 @@
+from typing import List, Tuple
+
+import numpy as np
+
+from pyobs.images import Image
 from pyobs.utils.guiding_stat.exposure_session_container import ExposureSessionContainer
 
 
@@ -6,3 +11,26 @@ class GuidingStatCalculator:
         self._stat_meta_class = stat_meta_class
         self._sessions = ExposureSessionContainer()
 
+    def init_stat(self, client: str) -> None:
+        self._sessions.init_session(client)
+
+    def _calc_rms(self, data: List[Tuple[float, float]]) -> tuple:
+        if len(data) == 0:
+            return ()
+
+        flattened_data = np.array(list(map(list, zip(*data))))
+        data_len = len(flattened_data[0])
+        rms = np.sqrt(np.sum(np.power(flattened_data, 2), axis=1)/data_len)
+        return tuple(rms)
+
+    def get_stat(self, client: str) -> Tuple[float, float]:
+        data = self._sessions.pop_session(client)
+        return self._calc_rms(data)
+
+    def add_data(self, image: Image):
+        if not image.has_meta(self._stat_meta_class):
+            raise KeyError("Image has not the necessary meta information!")
+
+        data = tuple(image.get_meta(self._stat_meta_class).__dict__.values())
+
+        self._sessions.add_data_to_all(data)
--- tests/utils/guiding_stat/test_guiding_stat_calculator.py
+++ tests/utils/guiding_stat/test_guiding_stat_calculator.py
@@ -1,17 +1,15 @@
 import pytest
 
 from pyobs.images import Image
-from pyobs.utils.guiding_stat.guiding_stat_calculator import GuidingStatCalculator
-from pyobs.images.meta import PixelOffsets, RaDecOffsets, AltAzOffsets
+from pyobs.images.meta import PixelOffsets, SkyOffsets
 from pyobs.utils.guiding_stat.exposure_session_container import ExposureSessionContainer
+from pyobs.utils.guiding_stat.guiding_stat_calculator import GuidingStatCalculator
 
 
 @pytest.fixture()
 def mock_meta_image():
     image = Image()
     image.set_meta(PixelOffsets(1.0, 1.0))
-    image.set_meta(RaDecOffsets(2.0, 2.0))
-    image.set_meta(AltAzOffsets(3.0, 3.0))
     return image
 
 
@@ -25,9 +23,24 @@ def test_pixel_offset(mock_meta_image):
     gsc = GuidingStatCalculator(PixelOffsets)
     gsc.init_stat("camera")
     gsc.add_data(mock_meta_image)
+    gsc.add_data(mock_meta_image)
     data = gsc.get_stat("camera")
 
     assert data == (1.0, 1.0)
 
     with pytest.raises(KeyError):
         gsc.get_stat("camera")
+
+
+def test_calc_rms():
+    gsc = GuidingStatCalculator(PixelOffsets)
+    assert gsc._calc_rms([]) == ()
+    assert gsc._calc_rms([(1.0, 1.0), (1.0, 7.0)]) == (1.0, 5.0)
+
+
+def test_missing_meta(mock_meta_image):
+    gsc = GuidingStatCalculator(SkyOffsets)
+    gsc.init_stat("camera")
+
+    with pytest.raises(KeyError):
+        gsc.add_data(mock_meta_image)

9610dd2dfce50c1c5fdcc64c5574de99b34eacaf
LeonMee <leon.meerwart@web.de>
2023-09-08 13:02:17 +0200

    added script for running pointing module

---
+++ pyobs/robotic/scripts/skyflats_pointing.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+import logging
+from typing import Any, Optional, Union, TYPE_CHECKING, Tuple
+
+from pyobs.interfaces import IBinning, ICamera, IWindow, IExposureTime, IImageType, IData, IModule
+from pyobs.robotic.scripts import Script
+from pyobs.utils.enums import ImageType
+
+if TYPE_CHECKING:
+    from pyobs.robotic import TaskSchedule, TaskArchive, TaskRunner
+
+log = logging.getLogger(__name__)
+
+
+class SkyflatsPointing(Script):
+    """Script for setting pointing via pointing module."""
+
+    def __init__(
+        self,
+        pointing: Union[str, IModule],
+        **kwargs: Any,
+    ):
+        """Init a new DarkBias script.
+        Args:
+
+        """
+        if "configuration" not in kwargs:
+            kwargs["configuration"] = {}
+        Script.__init__(self, **kwargs)
+
+        # store modules
+        self._pointing = pointing
+
+    async def can_run(self) -> bool:
+        """Whether this config can currently run.
+        Returns:
+            True if script can run now.
+        """
+        return True
+
+    async def run(
+        self,
+        task_runner: TaskRunner,
+        task_schedule: Optional[TaskSchedule] = None,
+        task_archive: Optional[TaskArchive] = None,
+    ) -> None:
+        """Run script.
+        Raises:
+            InterruptedError: If interrupted
+        """
+        pointing_module = await self.comm.proxy(self._pointing, IModule)
+        await pointing_module.run()
+
+
+__all__ = ["SkyflatsPointing"]

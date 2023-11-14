69d5af91a0e826ddf00f83ac70fa04fe9486b905
GermanHydrogen <admin@germanhydrogen.org>
2023-11-14 11:16:15 +0100

    Refactored StarExpTimeEstimator and added finer tests

--- pyobs/images/processors/exptime/star.py
+++ pyobs/images/processors/exptime/star.py
@@ -2,6 +2,9 @@ import logging
 from copy import copy
 from typing import Any
 
+import numpy as np
+from astropy.table import Table, Row
+
 from pyobs.images import Image
 from pyobs.images.processors.exptime.exptime import ExpTimeEstimator
 
@@ -12,6 +15,8 @@ class StarExpTimeEstimator(ExpTimeEstimator):
     """Estimate exposure time from a star."""
 
     __module__ = "pyobs.images.processors.exptime"
+
+    SATURATION = 50000
 
     def __init__(
         self,
@@ -33,7 +38,6 @@ class StarExpTimeEstimator(ExpTimeEstimator):
         self._edge = edge
         self._bias = bias
         self._saturated = saturated
-        self.coordinates = (None, None)
 
     async def _calc_exp_time(self, image: Image) -> float:
         """
@@ -43,40 +47,48 @@ class StarExpTimeEstimator(ExpTimeEstimator):
             image: Image to process.
         """
 
-        catalog = copy(image.catalog)
+        catalog = image.catalog
+        last_exp_time = image.header["EXPTIME"]
 
         if catalog is None:
             log.info("No catalog found in image.")
-            return image.header["EXPTIME"]
+            return last_exp_time
 
-        # sort catalog by peak flux
-        catalog.sort("peak", reverse=True)
+        max_peak = self._calc_max_peak(image)
 
-        # saturation level
-        if "DET-SATU" in image.header and "DET-GAIN" in image.header:
-            saturation = image.header["DET-SATU"] / image.header["DET-GAIN"]
-        else:
-            saturation = 50000
+        filtered_catalog = self._filter_saturated_stars(catalog, max_peak)
+        brightest_star = self._find_brightest_star(filtered_catalog)
 
-        # get max peak flux that we allow
-        max_peak = saturation * self._saturated
-
-        # filter out all stars that are saturated
-        catalog = catalog[catalog["peak"] <= max_peak]
-
-        # get brightest star, get its peak flux and store its coordinates
-        star = catalog[0]
-        peak = star["peak"]
-        log.info("Found peak of %.2f at %.1fx%.1f.", star["peak"], star["x"], star["y"])
-        self.coordinates = (star["x"], star["y"])
-
-        # get exposure time of image
-        exp_time = image.header["EXPTIME"]
-
-        # calculate new exposure time and return it
-        new_exp_time = exp_time / (peak - self._bias) * (max_peak - self._bias)
+        new_exp_time = self._calc_new_exp_time(last_exp_time, brightest_star["peak"], max_peak)
 
         return new_exp_time
+
+    def _calc_max_peak(self, image: Image) -> float:
+        saturation = self._calc_saturation_level_or_default(image)
+        return saturation * self._saturated
+
+    def _calc_saturation_level_or_default(self, image: Image) -> float:
+        if "DET-SATU" in image.header and "DET-GAIN" in image.header:
+            return image.header["DET-SATU"] / image.header["DET-GAIN"]
+
+        return self.SATURATION
+
+    @staticmethod
+    def _filter_saturated_stars(catalog: Table, max_peak: float) -> Table:
+        return catalog[catalog["peak"] <= max_peak]
+
+    @staticmethod
+    def _find_brightest_star(catalog: Table) -> Row:
+        brightest_star_index = np.argmax(catalog["peak"])
+        brightest_star = catalog[brightest_star_index]
+        return brightest_star
+
+    @staticmethod
+    def _log_brightest_star(star: Row):
+        log.info("Found peak of %.2f at %.1fx%.1f.", star["peak"], star["x"], star["y"])
+
+    def _calc_new_exp_time(self, exp_time, peak, max_peak):
+        return (max_peak - self._bias) / (peak - self._bias) * exp_time
 
 
 __all__ = ["StarExpTimeEstimator"]
--- tests/images/processors/exptime/test_star.py
+++ tests/images/processors/exptime/test_star.py
@@ -1,8 +1,9 @@
+import logging
+
 import pytest
 from astropy.table import QTable
 
 from pyobs.images import Image
-from pyobs.images.processors.detection import SourceDetection
 from pyobs.images.processors.exptime import StarExpTimeEstimator
 
 
@@ -29,17 +30,22 @@ async def test_full_wout_satu_header(mock_table):
     assert exp_time == 2.0
 
 
-@pytest.mark.asyncio
-async def test_full_with_satu_header(mock_table):
-    mock_image = Image(catalog=mock_table)
+def test_calc_saturation_level_or_default():
+    mock_image = Image()
 
-    mock_image.header["EXPTIME"] = 1.0
     mock_image.header["DET-SATU"] = 48000.0
     mock_image.header["DET-GAIN"] = 2.0
 
     estimator = StarExpTimeEstimator(saturated=0.1, bias=0.0)
 
-    exp_time = await estimator._calc_exp_time(mock_image)
+    assert estimator._calc_saturation_level_or_default(mock_image) == 24000.0
 
-    assert exp_time == 2400/2000
 
+
+def test_log_brightest_star(caplog):
+    star = {"peak": 10.0, "x": 40.0, "y": 30.0}
+
+    with caplog.at_level(logging.INFO):
+        StarExpTimeEstimator._log_brightest_star(star)
+
+    assert caplog.records[-1].message == "Found peak of 10.00 at 40.0x30.0."

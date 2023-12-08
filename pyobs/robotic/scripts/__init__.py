8ba3ea8a9c16e466977d2019a8e31dcf358e0d5c
Tim-Oliver Husser <thusser@uni-goettingen.de>
2023-12-08 19:43:06 +0100

    added imports

--- pyobs/robotic/scripts/__init__.py
+++ pyobs/robotic/scripts/__init__.py
@@ -1,2 +1,6 @@
 from .script import Script
 from .skyflats import SkyFlats
+from .darkbias import DarkBias
+from .sequential import SequentialRunner
+from .parallel import ParallelRunner
+from .selector import SelectorScript

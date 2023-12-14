03d0c8237df655479e4c1be844f74ce07ed7ecdd
germanhydrogen <admin@germanhydrogen.org>
2023-12-14 11:42:30 +0100

    Refactored sep photometry

---
+++ pyobs/images/processors/photometry/_sep_aperture_photometry.py
@@ -0,0 +1,71 @@
+from typing import List, Tuple, Optional
+
+import numpy as np
+
+from pyobs.images import Image
+from pyobs.images.processors.detection import SepSourceDetection
+
+
+class _SepAperturePhotometry:
+    def __init__(self, image: Image, positions: List[Tuple[float, float]]):
+        self._image = image.copy()
+        self._pos_x, self._pos_y = list(zip(*positions))
+
+        self._gain = image.header["DET-GAIN"] if "DET-GAIN" in image.header else None
+
+        self._data: Optional[np.ndarray] = None
+        self._average_background: Optional[np.ndarray] = None
+
+        self._calc_background()
+        self._update_background_header()
+
+    @property
+    def catalog(self):
+        return self._image.catalog
+
+    def _calc_background(self):
+        self._data, bkg = SepSourceDetection.remove_background(self._image.data, self._image.mask)
+        self._average_background = self._calc_average_background(bkg.back())
+
+    def _calc_average_background(self, background: np.ndarray) -> \
+    np.ndarray[float]:
+        """
+        since SEP sums up whole pixels, we need to do the same on an image of ones for the background_area
+        """
+        background_flux = self._sum_ellipse(background, self._image, self._pos_x, self._pos_y)
+        background_area = self._sum_ellipse(np.ones(shape=background.shape), self._image, self._pos_x, self._pos_y)
+
+        median_background = np.divide(background_flux, background_area, where=background_area != 0)
+        return median_background
+
+    def _update_background_header(self):
+        self._image.catalog[f"background"] = self._average_background
+
+    def __call__(self, diameter: int):
+        import sep
+        radius = self._calc_aperture_radius_in_px(diameter)
+        flux, fluxerr, _ = sep.sum_circle(
+            self._data, self._pos_x, self._pos_y, radius, mask=self._image.mask, err=self._image.uncertainty, gain=self._gain
+        )
+        self._update_flux_header(diameter, flux, fluxerr)
+
+    @staticmethod
+    def _sum_ellipse(data: np.ndarray, image: Image, x, y) -> np.ndarray[float]:
+        import sep
+        sum, _, _ = sep.sum_ellipse(
+            data, x, y,
+            image.catalog["a"],
+            image.catalog["b"],
+            theta=np.pi / 2.0,
+            r=2.5 * image.catalog["kronrad"],
+            subpix=1
+        )
+        return sum
+
+    def _calc_aperture_radius_in_px(self, diameter: int):
+        radius = diameter / 2.0
+        return radius / self._image.pixel_scale
+
+    def _update_flux_header(self, diameter: int, flux: np.ndarray, fluxerr: np.ndarray):
+        self._image.catalog[f"fluxaper{diameter}"] = flux
+        self._image.catalog[f"fluxerr{diameter}"] = fluxerr
--- pyobs/images/processors/photometry/pysep.py
+++ pyobs/images/processors/photometry/pysep.py
@@ -1,10 +1,10 @@
 import asyncio
-from typing import Any
 import logging
-import numpy as np
+from typing import Any
 
-from .photometry import Photometry
 from pyobs.images import Image
+from .photometry import Photometry
+from ._sep_aperture_photometry import _SepAperturePhotometry
 
 log = logging.getLogger(__name__)
 
@@ -14,40 +14,13 @@ class SepPhotometry(Photometry):
 
     __module__ = "pyobs.images.processors.photometry"
 
-    def __init__(
-        self,
-        threshold: float = 1.5,
-        minarea: int = 5,
-        deblend_nthresh: int = 32,
-        deblend_cont: float = 0.005,
-        clean: bool = True,
-        clean_param: float = 1.0,
-        **kwargs: Any,
-    ):
+    def __init__(self, **kwargs: Any):
         """Initializes a wrapper for SEP. See its documentation for details.
 
         Highly inspired by LCO's wrapper for SEP, see:
         https://github.com/LCOGT/banzai/blob/master/banzai/photometry.py
-
-        Args:
-            threshold: Threshold pixel value for detection.
-            minarea: Minimum number of pixels required for detection.
-            deblend_nthresh: Number of thresholds used for object deblending.
-            deblend_cont: Minimum contrast ratio used for object deblending.
-            clean: Perform cleaning?
-            clean_param: Cleaning parameter (see SExtractor manual).
-            *args:
-            **kwargs:
         """
         Photometry.__init__(self, **kwargs)
-
-        # store
-        self.threshold = threshold
-        self.minarea = minarea
-        self.deblend_nthresh = deblend_nthresh
-        self.deblend_cont = deblend_cont
-        self.clean = clean
-        self.clean_param = clean_param
 
     async def __call__(self, image: Image) -> Image:
         """Do aperture photometry on given image.
@@ -59,93 +32,32 @@ class SepPhotometry(Photometry):
             Image with attached catalog.
         """
         loop = asyncio.get_running_loop()
-        return await loop.run_in_executor(None, SepPhotometry._photometry, image)
+        return await loop.run_in_executor(None, self._photometry, image)
 
-    @staticmethod
-    def _photometry(image: Image) -> Image:
-        import sep
-        from pyobs.images.processors.detection import SepSourceDetection
-
-        # check data
+    def _photometry(self, image: Image) -> Image:
         if image.data is None:
             log.warning("No data found in image.")
             return image
+
         if image.catalog is None:
             log.warning("No catalog found in image.")
             return image
+        output_image = image.copy()
+        diameters = range(1, 9)
 
-        # no mask?
-        mask = image.mask if image.mask is not None else np.zeros(image.data.shape, dtype=bool)
+        if image.pixel_scale is not None:
+            for diameter in diameters:
+                output_image.catalog[f"fluxaper{diameter}"] = 0
+                output_image.catalog[f"fluxerr{diameter}"] = 0
 
-        # remove background
-        data, bkg = SepSourceDetection.remove_background(image.data, mask)
+        positions = [(x - 1, y - 1) for x, y in image.catalog.iterrows("x", "y")]
+        photometry = _SepAperturePhotometry(output_image, positions)
 
-        # fetch catalog
-        sources = image.catalog.copy()
-
-        # match SEP conventions
-        x, y = sources["x"] - 1, sources["y"] - 1
-
-        # get gain
-        gain = image.header["DET-GAIN"] if "DET-GAIN" in image.header else None
-
-        # perform aperture photometry for diameters of 1" to 8"
         for diameter in [1, 2, 3, 4, 5, 6, 7, 8]:
-            if image.pixel_scale is not None:
-                flux, fluxerr, flag = sep.sum_circle(
-                    data, x, y, diameter / 2.0 / image.pixel_scale, mask=image.mask, err=image.uncertainty, gain=gain
-                )
-                sources["fluxaper{0}".format(diameter)] = flux
-                sources["fluxerr{0}".format(diameter)] = fluxerr
+            photometry(diameter)
+        output_image.catalog = photometry.catalog
 
-            else:
-                sources["fluxaper{0}".format(diameter)] = 0
-                sources["fluxerr{0}".format(diameter)] = 0
-
-        # average background at each source
-        # since SEP sums up whole pixels, we need to do the same on an image of ones for the background_area
-        bkgflux, fluxerr, flag = sep.sum_ellipse(
-            bkg.back(), x, y, sources["a"], sources["b"], np.pi / 2.0, 2.5 * sources["kronrad"], subpix=1
-        )
-        background_area, _, _ = sep.sum_ellipse(
-            np.ones(shape=bkg.back().shape),
-            x,
-            y,
-            sources["a"],
-            sources["b"],
-            np.pi / 2.0,
-            2.5 * sources["kronrad"],
-            subpix=1,
-        )
-        sources["background"] = bkgflux
-        sources["background"][background_area > 0] /= background_area[background_area > 0]
-
-        # pick columns for catalog
-        new_columns = [
-            "fluxaper1",
-            "fluxerr1",
-            "fluxaper2",
-            "fluxerr2",
-            "fluxaper3",
-            "fluxerr3",
-            "fluxaper4",
-            "fluxerr4",
-            "fluxaper5",
-            "fluxerr5",
-            "fluxaper6",
-            "fluxerr6",
-            "fluxaper7",
-            "fluxerr7",
-            "fluxaper8",
-            "fluxerr8",
-            "background",
-        ]
-        cat = sources[image.catalog.colnames + new_columns]
-
-        # copy image, set catalog and return it
-        img = image.copy()
-        img.catalog = cat
-        return img
+        return output_image
 
 
 __all__ = ["SepPhotometry"]

54f9a5bf6880f15bd7b6773ec9bd18d3e9eca1d2
GermanHydrogen <admin@germanhydrogen.org>
2024-01-22 10:29:45 +0100

    Added missing unit tests

--- tests/images/processors/offsets/test_projected.py
+++ tests/images/processors/offsets/test_projected.py
@@ -1,6 +1,14 @@
+import logging
+from unittest.mock import Mock
+
 import numpy as np
+import pytest
+from photutils.datasets import make_gaussian_sources_image
 
 from scipy.signal.windows import gaussian
+
+from pyobs.images import Image
+from pyobs.images.meta import PixelOffsets
 from pyobs.images.processors.offsets import ProjectedOffsets
 
 
@@ -31,3 +39,111 @@ def test_subtract_sky_constant():
 
     subtracted_data = ProjectedOffsets._subtract_sky(data)
     np.testing.assert_array_almost_equal(subtracted_data, np.zeros(30), decimal=4)
+
+
+def test_process_axis_collapse():
+    offsets = ProjectedOffsets()
+    offsets._subtract_sky = Mock(return_value=np.ones(10))
+
+    image = Image(data=np.ones((10, 10)))
+    result = offsets._process(image)
+
+    np.testing.assert_array_equal(result[0], np.ones(10))
+    np.testing.assert_array_equal(result[1], np.ones(10))
+
+    np.testing.assert_array_equal(offsets._subtract_sky.call_args_list[0][0][0], np.ones(10) * 10)
+    np.testing.assert_array_equal(offsets._subtract_sky.call_args_list[1][0][0], np.ones(10) * 10)
+
+
+def test_process_invalid_timsec():
+    offsets = ProjectedOffsets()
+
+    image = Image(data=np.ones((10, 10)))
+    image.header["TRIMSEC"] = "INVALID"
+
+    with pytest.raises(ValueError):
+        offsets._process(image)
+
+
+def test_process_valid_timsec():
+    offsets = ProjectedOffsets()
+    offsets._subtract_sky = Mock(return_value=np.ones(10))
+
+    image = Image(data=np.ones((10, 10)))
+    image.header["TRIMSEC"] = "[2:9,2:9]"
+
+    offsets._process(image)
+
+    np.testing.assert_array_equal(offsets._subtract_sky.call_args_list[0][0][0], np.ones(8) * 8)
+    np.testing.assert_array_equal(offsets._subtract_sky.call_args_list[1][0][0], np.ones(8) * 8)
+
+
+@pytest.mark.asyncio
+async def test_call_ref(mocker, caplog):
+    offsets = ProjectedOffsets()
+    mocker.patch.object(offsets, "_process", return_value=(np.ones(10)*10, np.ones(10)*10))
+    image = Image(data=np.ones((10, 10)))
+
+    with caplog.at_level(logging.INFO):
+        assert await offsets(image) == image
+
+    np.testing.assert_array_equal(offsets._ref_image[0], np.ones(10) * 10)
+    np.testing.assert_array_equal(offsets._ref_image[1], np.ones(10) * 10)
+    assert caplog.messages[0] == "Initialising auto-guiding with new image..."
+
+
+@pytest.mark.asyncio
+async def test_call_no_corr(mocker, caplog):
+    offsets = ProjectedOffsets()
+    offsets._ref_image = (np.ones(10) * 10, np.ones(10) * 10)
+    mocker.patch.object(offsets, "_process", return_value=(np.ones(10) * 10, np.ones(10) * 10))
+    mocker.patch.object(offsets, "_correlate", return_value=None)
+    image = Image(data=np.ones((10, 10)))
+
+    with caplog.at_level(logging.INFO):
+        assert await offsets(image) == image
+
+    assert caplog.messages[0] == "Perform auto-guiding on new image..."
+    assert caplog.messages[1] == "Could not correlate peaks."
+
+
+@pytest.mark.asyncio
+async def test_call_no_corr(mocker):
+    offsets = ProjectedOffsets()
+    offsets._ref_image = (np.ones(10) * 10, np.ones(10) * 10)
+    mocker.patch.object(offsets, "_process", return_value=(np.ones(10) * 10, np.ones(10) * 10))
+    mocker.patch.object(offsets, "_correlate", return_value=10)
+    image = Image(data=np.ones((10, 10)))
+
+    result = await offsets(image)
+
+    assert result.get_meta(PixelOffsets).dx == 10
+    assert result.get_meta(PixelOffsets).dy == 10
+
+'''
+@pytest.mark.asyncio
+async def test_integration():
+    table = QTable()
+    table['amplitude'] = [150, 150]
+    table['x_mean'] = [15, 30]
+    table['y_mean'] = [15, 35]
+    table['x_stddev'] = [1, 3]
+    table['y_stddev'] = [1, 3]
+    table['theta'] = np.radians(np.array([0, 0]))
+    size = (32, 64)
+    signal = make_gaussian_sources_image(size, table)
+    data = signal + np.ones(size) * 2
+
+    img_data = np.pad(data, ((10, 0), (0, 0)), mode='constant', constant_values=0.0)
+    ref_data = np.pad(data, ((0, 10), (0, 0)), mode='constant', constant_values=0.0)  # Pad behind signal, to match data length
+
+    ref_image = Image(data=ref_data)
+    image = Image(data=img_data)
+    offsets = ProjectedOffsets()
+
+    assert await offsets(ref_image) == ref_image
+    result = await offsets(image)
+
+    assert result.get_meta(PixelOffsets).dy == 10.0
+    assert result.get_meta(PixelOffsets).dx == 0.0
+'''

f0a3f5ffe455a42b64a193be9b23e35e625712bb
GermanHydrogen <admin@germanhydrogen.org>
2024-03-27 11:23:59 +0100

    Refactoreesisting g schedular unit test

--- tests/modules/robotic/test_scheduler.py
+++ tests/modules/robotic/test_scheduler.py
@@ -1,67 +1,59 @@
-import time
+from typing import List
 
-from astroplan import ObservingBlock, FixedTarget
 import astropy.units as u
+import pytest
+from astroplan import ObservingBlock, FixedTarget
 from astropy.coordinates import SkyCoord
 
 from pyobs.modules.robotic import Scheduler
 
 
-def test_compare_block_lists():
-    # create lists of blocks
-    blocks = []
-    for i in range(10):
-        blocks.append(
-            ObservingBlock(
-                FixedTarget(SkyCoord(0.0 * u.deg, 0.0 * u.deg, frame="icrs"), name=str(i)), 10 * u.minute, 10
-            )
+@pytest.fixture
+def schedule_blocks() -> List[ObservingBlock]:
+    blocks = [
+        ObservingBlock(
+            FixedTarget(SkyCoord(0.0 * u.deg, 0.0 * u.deg, frame="icrs"), name=str(i)), 10 * u.minute, 10
         )
+        for i in range(10)
+    ]
 
-    # create two lists from these with some overlap
-    blocks1 = blocks[:7]
-    blocks2 = blocks[5:]
+    return blocks
 
-    # compare
-    unique1, unique2 = Scheduler._compare_block_lists(blocks1, blocks2)
 
-    # get names
-    names1 = [int(b.target.name) for b in unique1]
-    names2 = [int(b.target.name) for b in unique2]
+def test_compare_block_lists_with_overlap(schedule_blocks):
+    old_blocks = schedule_blocks[:7]
+    new_blocks = schedule_blocks[5:]
 
-    # names1 should contain 0, 1, 2, 3, 4
-    assert set(names1) == {0, 1, 2, 3, 4}
+    removed, added = Scheduler._compare_block_lists(old_blocks, new_blocks)
 
-    # names2 should contain 7, 8, 9
-    assert set(names2) == {7, 8, 9}
+    removed_names = [int(b.target.name) for b in removed]
+    new_names = [int(b.target.name) for b in added]
 
-    # create two lists from these with no overlap
-    blocks1 = blocks[:5]
-    blocks2 = blocks[5:]
+    assert set(removed_names) == {0, 1, 2, 3, 4}
+    assert set(new_names) == {7, 8, 9}
 
-    # compare
-    unique1, unique2 = Scheduler._compare_block_lists(blocks1, blocks2)
 
-    # get names
-    names1 = [int(b.target.name) for b in unique1]
-    names2 = [int(b.target.name) for b in unique2]
+def test_compare_block_lists_without_overlap(schedule_blocks):
+    old_blocks = schedule_blocks[:5]
+    new_blocks = schedule_blocks[5:]
 
-    # names1 should contain 0, 1, 2, 3, 4
-    assert set(names1) == {0, 1, 2, 3, 4}
+    removed, added = Scheduler._compare_block_lists(old_blocks, new_blocks)
 
-    # names2 should contain 5, 6, 7, 8, 9
-    assert set(names2) == {5, 6, 7, 8, 9}
+    removed_names = [int(b.target.name) for b in removed]
+    new_names = [int(b.target.name) for b in added]
 
-    # create two identical lists
-    blocks1 = blocks
-    blocks2 = blocks
+    assert set(removed_names) == {0, 1, 2, 3, 4}
+    assert set(new_names) == {5, 6, 7, 8, 9}
 
-    # compare
-    unique1, unique2 = Scheduler._compare_block_lists(blocks1, blocks2)
 
-    # get names
-    names1 = [int(b.target.name) for b in unique1]
-    names2 = [int(b.target.name) for b in unique2]
+def test_compare_block_lists_identical(schedule_blocks):
+    old_blocks = schedule_blocks
+    new_blocks = schedule_blocks
 
-    # both lists should be empty
-    assert len(names1) == 0
-    assert len(names2) == 0
+    removed, added = Scheduler._compare_block_lists(old_blocks, new_blocks)
+
+    removed_names = [int(b.target.name) for b in removed]
+    new_names = [int(b.target.name) for b in added]
+
+    assert len(removed_names) == 0
+    assert len(new_names) == 0

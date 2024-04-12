1e63598ae37cd7e8ce3a7111a7822b3cfae716d8
LeonMee <leon.meerwart@web.de>
2024-04-12 11:16:29 +0200

    removed and modified comments and docstring

--- pyobs/images/processors/misc/central_filter.py
+++ pyobs/images/processors/misc/central_filter.py
@@ -18,7 +18,7 @@ class CentralFilter(ImageProcessor):
         """Init an image processor that filters out everything except for a central circle.
 
         Args:
-            radius: radius of the central circle (in pixels by default)
+            radius: radius of the central circle in pixels
             center: fits-header keywords defining the pixel coordinates of the center of the circle
         """
         ImageProcessor.__init__(self, **kwargs)
@@ -28,27 +28,23 @@ class CentralFilter(ImageProcessor):
         self._radius = radius
 
     async def __call__(self, image: Image) -> Image:
-        """Remove background from image.
+        """Remove everything outside the given radius from the image.
 
         Args:
-            image: Image to filter.
+            image: Image to mask.
 
         Returns:
-            Filtered Image.
+            Masked Image.
         """
 
-        # get pixels to acquire on
         center_x, center_y = image.header[self._center[0]], image.header[self._center[1]]
 
-        # filter everything except for central circle
         radius = self._radius
 
         # create arrays with the pixel coordinates of image.data as entries
         nx, ny = image.data.shape
         data_x = np.array([np.arange(nx) for y in range(ny)])
         data_y = np.array([y + np.zeros(nx) for y in range(ny)])
-
-        # create filter mask
         circ_mask = (data_x - center_x) ** 2 + (data_y - center_y) ** 2 <= radius**2
 
         image.data *= circ_mask.transpose()

0a7403a4a49249e0931c85cc875e06d764a9c086
Tim-Oliver Husser <thusser@uni-goettingen.de>
2025-08-08 12:42:16 +0200

    added SaveImage to write JPG, PNG, etc

--- pyobs/images/processors/image/__init__.py
+++ pyobs/images/processors/image/__init__.py
@@ -3,5 +3,6 @@ from .download import Download
 from .grayscale import Grayscale
 from .normalize import Normalize
 from .save import Save
+from .saveimage import SaveImage
 from .smooth import Smooth
 from .softbin import SoftBin
---
+++ pyobs/images/processors/image/saveimage.py
@@ -0,0 +1,58 @@
+import io
+import logging
+import os.path
+from typing import Any
+
+from pyobs.images.processor import ImageProcessor
+from pyobs.images import Image
+from pyobs.utils.fits import FilenameFormatter
+
+log = logging.getLogger(__name__)
+
+
+class SaveImage(ImageProcessor):
+    """Broadcast image."""
+
+    __module__ = "pyobs.images.processors.misc"
+
+    def __init__(self, filename: str = "/pyobs/image.jpg", format: str | None = None, **kwargs: Any):
+        """Init an image processor that saves an image as jpeg
+
+        Args:
+            filename: Filename to broadcast image.
+            format: Explicitly set the image format to use.
+        """
+        ImageProcessor.__init__(self, **kwargs)
+
+        self._formatter = FilenameFormatter(filename)
+        self._image_format = format
+
+    async def __call__(self, image: Image) -> Image:
+        """Save image as jpeg.
+
+        Args:
+            image: Image to save.
+
+        Returns:
+            Original image.
+        """
+        from pyobs.images.processors.annotation._pil import from_image
+
+        filename = image.format_filename(self._formatter)
+
+        image_format = self._image_format
+        if image_format is None:
+            image_format = os.path.splitext(filename)[1][1:].upper()
+            if image_format == "JPG":
+                image_format = "JPEG"
+        print(image_format)
+
+        im = from_image(image)
+        with io.BytesIO() as bio:
+            im.save(bio, format=image_format)
+            await self.vfs.write_bytes(filename, bio.getvalue())
+
+        return image
+
+
+__all__ = ["SaveImage"]

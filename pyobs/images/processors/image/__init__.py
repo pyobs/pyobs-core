a3a52319c2a9c5bdc123f542bd3af338b576feab
Tim-Oliver Husser <thusser@uni-goettingen.de>
2025-08-08 12:15:58 +0200

    added Normalize

--- pyobs/images/processors/image/__init__.py
+++ pyobs/images/processors/image/__init__.py
@@ -1,6 +1,7 @@
 from .addfitsheaders import AddFitsHeaders
 from .download import Download
 from .grayscale import Grayscale
+from .normalize import Normalize
 from .save import Save
 from .smooth import Smooth
 from .softbin import SoftBin

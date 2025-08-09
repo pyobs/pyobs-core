a82a719dcebdbe9ed1ddf2a78f2aa2102b643fe1
Tim-Oliver Husser <thusser@uni-goettingen.de>
2025-08-09 21:00:08 +0200

    new GetFitsHeaders for fetching FITS headers from other modules

---
+++ pyobs/images/processors/modules/getfitsheaders.py
@@ -0,0 +1,59 @@
+import asyncio
+import logging
+from asyncio import Task
+from typing import Any
+
+from pyobs.images.processor import ImageProcessor
+from pyobs.images import Image
+from pyobs.interfaces import IFitsHeaderBefore
+
+log = logging.getLogger(__name__)
+
+
+class GetFitsHeaders(ImageProcessor):
+    """Retrieves FITS headers from another module (or more)."""
+
+    __module__ = "pyobs.images.processors.modules"
+
+    def __init__(self, sender: str | list[str], namespace: str | None = None, **kwargs: Any):
+        """Init a fits header processor.
+
+        Args:
+            sender: Sender(s) to fetch FITS headers from.
+            namespace: FITS namespace.
+        """
+        ImageProcessor.__init__(self, **kwargs)
+
+        self._senders = [sender] if isinstance(sender, str) else sender
+        self._namespace = namespace
+
+    async def __call__(self, image: Image) -> Image:
+        """Retrieve FITS headers from another module (or more).
+
+        Args:
+            image: Image to add headers to.
+
+        Returns:
+            Image with headers added.
+        """
+
+        # loop all modules
+        requests: list[Task[Any]] = []
+        for sender in self._senders:
+            # get proxy
+            proxy = await self.proxy(sender, IFitsHeaderBefore)
+
+            # request headers
+            requests.append(asyncio.create_task(proxy.get_fits_header_before(self._namespace)))
+
+        # copy image
+        out = image.copy()
+
+        # await all requests
+        for request in requests:
+            hdr = await request
+            out.header.extend(hdr)
+        return out
+
+
+__all__ = ["GetFitsHeaders"]

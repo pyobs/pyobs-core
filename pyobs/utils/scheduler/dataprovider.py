8564d0260aacb03d112823c37a01953178b3a083
Tim-Oliver Husser <thusser@uni-goettingen.de>
2025-10-24 14:03:18 +0200

    testing

--- pyobs/utils/scheduler/dataprovider.py
+++ pyobs/utils/scheduler/dataprovider.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
-from astropy.time import Time
+from functools import cache
+from astroplan import Observer
+from astropy.time import Time, TimeDelta
+import astropy.units as u
 
 from pyobs.robotic import Task
 
@@ -13,29 +16,30 @@ class TaskSuccess:
 class DataProvider:
     """Data provider for Merit classes."""
 
-    def __init__(self) -> None:
-        self._task_success: dict[Task, list[TaskSuccess]] = {}
+    def __init__(self, observer: Observer):
+        self.observer = observer
 
     def reset(self) -> None:
         """Reset data provider."""
-        self._task_success.clear()
+        ...
 
-    def get_night(self) -> Time:
-        """Returns the current night."""
-        return Time.now()
+    @cache
+    def get_night(self, time: Time) -> Time:
+        """Returns the night for the given time."""
+        night = Time(f"{time.year}-{time.month:02d}-{time.day:02d} 00:00:00")
+        if time.hour < 12:
+            night -= TimeDelta(1.0 * u.day)
+        return night
 
-    def get_task_success_count(self, task: Task) -> int:
+    @cache
+    def get_task_success_count(self, time: Time, task: Task) -> int:
         """Return the number of successful runs for task."""
-        if task not in self._task_success:
-            return 0
-        return len(self._task_success[task])
+        return 0
 
+    @cache
     def get_task_success(self, task: Task, number: int = -1) -> TaskSuccess | None:
         """Return the number of successful runs for task."""
-        try:
-            return self._task_success[task][number]
-        except IndexError:
-            return None
+        return None
 
 
 __all__ = ["DataProvider"]

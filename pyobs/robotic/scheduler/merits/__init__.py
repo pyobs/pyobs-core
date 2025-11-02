3b5f71b752c146251ceb039fe90914776e27b6c7
Tim-Oliver Husser <thusser@uni-goettingen.de>
2025-11-02 21:43:14 +0100

    added BeforeTime and AfterTime merits and tests

--- pyobs/robotic/scheduler/merits/__init__.py
+++ pyobs/robotic/scheduler/merits/__init__.py
@@ -1,4 +1,6 @@
 from .merit import Merit
+from .aftertime import AfterTimeMerit
+from .beforetime import BeforeTimeMerit
 from .constant import ConstantMerit
 from .pernight import PerNightMerit
 from .random import RandomMerit
---
+++ pyobs/robotic/scheduler/merits/aftertime.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from typing import Any, TYPE_CHECKING
+from .merit import Merit
+
+if TYPE_CHECKING:
+    from astropy.time import Time
+    from pyobs.robotic import Task
+    from ..dataprovider import DataProvider
+
+
+class AfterTimeMerit(Merit):
+    """Merit function that gives 1 after a given time."""
+
+    def __init__(self, after: Time, **kwargs: Any):
+        super().__init__(**kwargs)
+        self._after = after
+
+    def __call__(self, time: Time, task: Task, data: DataProvider) -> float:
+        return 1.0 if time >= self._after else 0.0
+
+
+__all__ = ["AfterTimeMerit"]

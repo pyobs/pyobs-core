62af63d2fbad54a2f85f83ccc7154d547b9a991f
Tim-Oliver Husser <thusser@uni-goettingen.de>
2025-11-19 15:25:29 +0100

    reorganized

--- pyobs/utils/grids/filters.py
+++ pyobs/utils/grids/filters.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 import abc
 from typing import Any
 from astropy.coordinates import SkyCoord
+import astropy.units as u
+from astropy.time import Time
 
 from .gridnode import GridNode
 from .grid import Grid
@@ -86,4 +88,36 @@ class GridFilterValue(GridFilter):
             return point
 
 
-__all__ = ["GridFilter", "GridFilterValue"]
+class ConvertGridToSkyCoord(GridFilter):
+    def __init__(self, grid: Grid | GridFilter, frame: str = "altaz", **kwargs: object):
+        GridFilter.__init__(self, grid, **kwargs)
+        self._frame = frame
+
+    def _get_next(self) -> tuple[float, float] | SkyCoord:
+        """Returns the points of a new grid."""
+
+        point = next(self._grid)
+        if not isinstance(point, tuple) or len(point) != 2:
+            raise TypeError(f"Expected a tuple with 2 elements, got {type(point)}")
+
+        # to SkyCoord
+        return SkyCoord(
+            point[0] * u.deg, point[1] * u.deg, frame=self._frame, location=self.location, obstime=Time.now()
+        )
+
+
+class ConvertGridFrame(GridFilter):
+    def __init__(self, grid: Grid | GridFilter, frame: str = "altaz", **kwargs: object):
+        GridFilter.__init__(self, grid, **kwargs)
+        self._frame = frame
+
+    def _get_next(self) -> tuple[float, float] | SkyCoord:
+        """Returns the points of a new grid."""
+
+        point = next(self._grid)
+        if not isinstance(point, SkyCoord):
+            raise TypeError("Expected a SkyCoord.")
+        return point.transform_to(frame=self._frame)
+
+
+__all__ = ["GridFilterValue", "ConvertGridFrame", "ConvertGridToSkyCoord"]
--- pyobs/utils/grids/grid.py
+++ pyobs/utils/grids/grid.py
@@ -1,6 +1,7 @@
 import abc
 from typing import Any
 from astropy.coordinates import SkyCoord
+import numpy as np
 
 from .gridnode import GridNode
 
@@ -27,3 +28,63 @@ class Grid(GridNode, metaclass=abc.ABCMeta):
 
     def log_last(self) -> None:
         self.log(self._last)
+
+
+class RegularSphericalGrid(Grid):
+    def __init__(self, n_lon: int, n_lat: int, **kwargs: Any):
+        """Creates a grid with points at the intersections of longitudinal and latitudinal lines.
+
+        Params:
+            n_lon: Number of longitudinal points.
+            n_lat: Number of latitudinal points.
+
+        Returns:
+            Lat/lon grid.
+        """
+        points: list[tuple[float, float]] = []
+        for lon in np.linspace(0.0, 360.0 - 360.0 / n_lon, n_lon):
+            for lat in np.linspace(-90.0, 90.0, n_lat):
+                points.append((float(lon), float(lat)))
+        Grid.__init__(self, points=points, **kwargs)
+
+
+class GraticuleSphericalGrid(Grid):
+    def __init__(self, n: int, **kwargs: Any):
+        """Creates equidistributed points on the surface of a sphere
+
+        See https://www.cmu.edu/biolphys/deserno/pdf/sphere_equi.pdf
+
+        Params:
+            n:  Number of points
+
+        Returns:
+            Lat/lon grid.
+        """
+
+        # init
+        a = 4 * np.pi / n
+        d = np.sqrt(a)
+        m_phi = round(np.pi / d)
+        d_phi = np.pi / m_phi
+        d_varphi = a / d_phi
+        points: list[tuple[float, float]] = []
+
+        # conversion radians -> degrees
+        r2d = 180.0 / np.pi
+
+        # loop latitudinal
+        for m in range(0, m_phi):
+            lat = np.pi * (m + 0.5) / m_phi
+            m_varphi = round(2 * np.pi * np.sin(lat) / d_varphi)
+
+            # loop longitudinal
+            for n in range(0, m_varphi):
+                lon = 2 * np.pi * n / m_varphi
+
+                # append to grid
+                points.append((lon * r2d, lat * r2d - 90.0))
+
+        Grid.__init__(self, points=points, **kwargs)
+
+
+__all__ = ["RegularSphericalGrid", "GraticuleSphericalGrid"]

8f82256d099e1acf8b50a1143d1e49b1e3ad63a6
Tim-Oliver Husser <thusser@uni-goettingen.de>
2025-12-27 18:54:10 +0100

    made it an interface...

--- pyobs/utils/accesscontrol/__init__.py
+++ pyobs/utils/accesscontrol/__init__.py
@@ -1,13 +1,16 @@
+from abc import ABCMeta, abstractmethod
+
 from pyobs.object import Object
 
 
-class AccessControl(Object):
-    """Base class for accesscontrol."""
+class AccessControl(Object, metaclass=ABCMeta):
+    """Abstract base class for accesscontrol."""
 
+    @abstractmethod
     def has_access(self, client: str, module: str, method: str) -> bool:
         """Check if a client has access to a method.
 
-        This method *must* be implemented by subclasses, since this implementation always returns True.
+        This method *must* be implemented by subclasses!
 
         Args:
             client: Name of client.
@@ -17,4 +20,4 @@ class AccessControl(Object):
         Returns:
             Whether the given client is allowed to call the given method on the current module.
         """
-        return True
+        ...

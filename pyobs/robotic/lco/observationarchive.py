fc0f4485a8bcd8c1cbdba04d16fc28682b81ef1b
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-01-14 16:23:15 +0100

    return ObservationList

--- pyobs/robotic/lco/observationarchive.py
+++ pyobs/robotic/lco/observationarchive.py
@@ -1,9 +1,10 @@
+import datetime
 from typing import Any
 
 from .portal import Portal
 from .task import LcoTask
 from ..task import Task
-from ..observation import Observation, ObservationState
+from ..observation import Observation, ObservationState, ObservationList
 from ..observationarchive import ObservationArchive
 
 
@@ -27,7 +28,7 @@ class LcoObservationArchive(ObservationArchive):
         # portal
         self._portal = Portal(url, token)
 
-    async def observations_for_task(self, task: Task) -> list[Observation]:
+    async def observations_for_task(self, task: Task) -> ObservationList:
         """Returns list of observations for the given task.
 
         Args:
@@ -52,7 +53,18 @@ class LcoObservationArchive(ObservationArchive):
                     state=STATE_MAP[obs.state],
                 )
             )
-        return observations
+        return ObservationList(observations)
+
+    async def observations_for_night(self, date: datetime.date) -> ObservationList:
+        """Returns list of observations for the given task.
+
+        Args:
+            date: Date of night to get observations for.
+
+        Returns:
+            List of observations for the given task.
+        """
+        return ObservationList()
 
 
 __all__ = ["LcoObservationArchive"]

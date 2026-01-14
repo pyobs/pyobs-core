6ff84011149c720bf328dd4377016fcdfbe463dd
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-01-14 16:17:55 +0100

    added observations_for_night

--- pyobs/robotic/scheduler/observationarchiveevolution.py
+++ pyobs/robotic/scheduler/observationarchiveevolution.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
+import datetime
 from typing import TYPE_CHECKING
 from uuid import uuid4
+from astroplan import Observer
 
 from pyobs.robotic.observation import ObservationState, ObservationList
+from ...utils.time import Time
 
 if TYPE_CHECKING:
     from pyobs.robotic import ScheduledTask, Task, Observation
@@ -10,9 +13,11 @@ if TYPE_CHECKING:
 
 
 class ObservationArchiveEvolution:
-    def __init__(self, obs_archive: ObservationArchive | None = None):
+    def __init__(self, observer: Observer, obs_archive: ObservationArchive | None = None):
         self._obs_archive = obs_archive
         self._obs_for_task: dict[Task, ObservationList] = {}
+        self._obs_for_night: dict[datetime.date, ObservationList] = {}
+        self._observer = observer
 
     async def evolve(self, scheduled_task: ScheduledTask) -> None:
         obs = Observation(
@@ -26,12 +31,31 @@ class ObservationArchiveEvolution:
         await self.observations_for_task(scheduled_task.task)
         self._obs_for_task[scheduled_task.task].append(obs)
 
+        night = Time.now().night_obs(self._observer)
+        await self.observations_for_night(night)
+        self._obs_for_night[night].append(obs)
+
     async def observations_for_task(self, task: Task) -> ObservationList:
         if task not in self._obs_for_task:
             self._obs_for_task[task] = (
                 ObservationList() if self._obs_archive is None else await self._obs_archive.observations_for_task(task)
             )
         return self._obs_for_task[task]
+
+    async def observations_for_night(self, date: datetime.date) -> ObservationList:
+        """Returns list of observations for the given task.
+
+        Args:
+            date: Date of night to get observations for.
+
+        Returns:
+            List of observations for the given task.
+        """
+        if date not in self._obs_for_night:
+            self._obs_for_night[date] = (
+                ObservationList() if self._obs_archive is None else await self._obs_archive.observations_for_night(date)
+            )
+        return self._obs_for_night[date]
 
 
 __all__ = ["ObservationArchiveEvolution"]

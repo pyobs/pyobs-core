f257b605cb652600d21d81a8872735360adf6902
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-01-16 15:20:50 +0100

    added create method

--- pyobs/robotic/scheduler/constraints/constraint.py
+++ pyobs/robotic/scheduler/constraints/constraint.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 import astroplan
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+
+from pyobs.object import create_object
 
 if TYPE_CHECKING:
     from astropy.time import Time
@@ -15,3 +17,23 @@ class Constraint(metaclass=ABCMeta):
 
     @abstractmethod
     async def __call__(self, time: Time, task: Task, data: DataProvider) -> bool: ...
+
+    @staticmethod
+    def create(config: Constraint | dict[str, Any]) -> Constraint:
+        if isinstance(config, Constraint):
+            return config
+        else:
+            from . import __all__ as constraints
+
+            constraints_lower = [c.lower() for c in constraints]
+            try:
+                idx = constraints_lower.index(config["type"].lower() + "constraint")
+            except ValueError:
+                raise ValueError(f"Invalid constraint type: {config['type']}")
+
+            config["class"] = f"pyobs.robotic.scheduler.constraints.{constraints[idx]}"
+            obj = create_object(config)
+            if isinstance(obj, Constraint):
+                return obj
+            else:
+                raise ValueError(f"Invalid constraint config: {config}")

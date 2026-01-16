5ee6c5d9f0cc487e157f36fca97aacb4ce9661e1
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-01-16 17:33:34 +0100

    added create methos

--- pyobs/robotic/scheduler/merits/merit.py
+++ pyobs/robotic/scheduler/merits/merit.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from abc import ABCMeta, abstractmethod
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+
+from pyobs.object import create_object
 
 if TYPE_CHECKING:
     from astropy.time import Time
@@ -13,6 +15,28 @@ class Merit(metaclass=ABCMeta):
 
     @abstractmethod
     async def __call__(self, time: Time, task: Task, data: DataProvider) -> float: ...
+
+    @staticmethod
+    def create(config: Merit | dict[str, Any]) -> Merit:
+        if isinstance(config, Merit):
+            return config
+        else:
+            from . import __all__ as constraints
+
+            if "." not in config["class"]:
+                constraints_lower = [c.lower() for c in constraints]
+                try:
+                    idx = constraints_lower.index(config["class"].lower() + "merit")
+                except ValueError:
+                    raise ValueError(f"Invalid merit type: {config['class']}")
+
+                config["class"] = f"pyobs.robotic.scheduler.merits.{constraints[idx]}"
+
+            obj = create_object(config)
+            if isinstance(obj, Merit):
+                return obj
+            else:
+                raise ValueError(f"Invalid merit config: {config}")
 
 
 __all__ = ["Merit"]

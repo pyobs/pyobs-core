fa6e69c3a2a2de0da5415321d40e6503902e7a93
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-02-03 13:52:04 +0100

    class -> type

--- pyobs/robotic/scheduler/merits/merit.py
+++ pyobs/robotic/scheduler/merits/merit.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 from typing import TYPE_CHECKING, Any
+from astropy.time import Time
 
-from pyobs.object import create_object
 from pyobs.utils.serialization import SubClassBaseModel
 
 if TYPE_CHECKING:
-    from astropy.time import Time
     from ..dataprovider import DataProvider
     from pyobs.robotic import Task
 
@@ -24,16 +23,16 @@ class Merit(SubClassBaseModel, metaclass=ABCMeta):
         elif "type" in config:
             from . import __all__ as constraints
 
-            if "." not in config["class"]:
+            if "." not in config["type"]:
                 constraints_lower = [c.lower() for c in constraints]
                 try:
-                    idx = constraints_lower.index(config["class"].lower() + "merit")
+                    idx = constraints_lower.index(config["type"].lower() + "merit")
                 except ValueError:
-                    raise ValueError(f"Invalid merit type: {config['class']}")
+                    raise ValueError(f"Invalid merit type: {config['type']}")
 
                 config["class"] = f"pyobs.robotic.scheduler.merits.{constraints[idx]}"
 
-            obj = create_object(config)
+            obj = Merit.model_validate(config, by_alias=True)
             if isinstance(obj, Merit):
                 return obj
             else:

e0a44498655f766d54268ff8aba03465820b4d1b
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-04-30 18:30:34 +0200

    added min_elevation

--- pyobs/robotic/scheduler/constraints/solarelevationconstraint.py
+++ pyobs/robotic/scheduler/constraints/solarelevationconstraint.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 import astroplan
 import astropy.units as u
+import logging
 from .constraint import Constraint
 
 if TYPE_CHECKING:
@@ -10,17 +11,23 @@ if TYPE_CHECKING:
     from pyobs.robotic import Task
 
 
+log = logging.getLogger(__name__)
+
+
 class SolarElevationConstraint(Constraint):
     """Solar elevation constraint."""
 
+    min_elevation: float = -90.0
     max_elevation: float
 
     def to_astroplan(self) -> astroplan.AtNightConstraint:
+        if self.min_elevation > -90.0:
+            log.warning("Minimum solar elevation constraint not supported by astroplan.")
         return astroplan.AtNightConstraint(max_solar_altitude=self.max_elevation * u.deg)
 
     async def __call__(self, time: Time, task: Task, data: DataProvider) -> bool:
         sun = data.observer.sun_altaz(time)
-        return float(sun.alt.degree) <= self.max_elevation
+        return self.min_elevation <= float(sun.alt.degree) <= self.max_elevation
 
 
 __all__ = ["SolarElevationConstraint"]

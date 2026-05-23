07a98f12ba2f5b70835fada503225e93a4d61cdb
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-05-23 18:43:36 +0200

    added logging

--- pyobs/utils/http.py
+++ pyobs/utils/http.py
@@ -1,14 +1,17 @@
 import asyncio
 from typing import Any
-
+import logging
 import aiohttp
-from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type
+from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type, before_sleep_log
+
+log = logging.getLogger(__name__)
 
 
 @retry(
     retry=retry_if_exception_type((aiohttp.ClientError, asyncio.TimeoutError)),
     wait=wait_exponential(multiplier=1, min=1, max=30),
     stop=stop_after_attempt(5),
+    before_sleep=before_sleep_log(log, logging.WARNING),  # type: ignore[arg-type]
     reraise=True,
 )
 async def http_request_with_retries(

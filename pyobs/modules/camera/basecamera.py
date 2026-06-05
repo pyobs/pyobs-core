a12c880710a5dbf175f2799b519a13daa32af420
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-06-05 09:57:27 +0200

    make ruff happy

--- pyobs/modules/camera/basecamera.py
+++ pyobs/modules/camera/basecamera.py
@@ -435,23 +435,24 @@ class BaseCamera(Module, ImageFitsHeaderMixin, ICamera, IExposureTime, IImageTyp
         is_bottom_binned = np.ceil((is_bottom - hdr["YORGSUBF"]) / hdr["YBINNING"])
 
         # set it
-        hdr["TRIMSEC"] = ("[%d:%d,%d:%d]" % (is_left_binned, is_right_binned, is_top_binned, is_bottom_binned), c2)
-        hdr["DATASEC"] = ("[%d:%d,%d:%d]" % (is_left_binned, is_right_binned, is_top_binned, is_bottom_binned), c2)
+        sec = f"[{is_left_binned:.0f}:{is_right_binned:.0f},{is_top_binned:.0f}:{is_bottom_binned:.0f}]"
+        hdr["TRIMSEC"] = (sec, c2)
+        hdr["DATASEC"] = (sec, c2)
 
         # now get BIASSEC -- whatever we do, we only take the last (!) one
         # which axis?
         if img_left + img_width > left + width:
             left_binned = np.floor((is_right - hdr["XORGSUBF"]) / hdr["XBINNING"]) + 1
-            hdr["BIASSEC"] = ("[%d:%d,1:%d]" % (left_binned, hdr["NAXIS1"], hdr["NAXIS2"]), c1)
+            hdr["BIASSEC"] = ("[{:.0f}:{},1:{}]".format(left_binned, hdr["NAXIS1"], hdr["NAXIS2"]), c1)
         elif img_left < left:
             right_binned = np.ceil((is_left - hdr["XORGSUBF"]) / hdr["XBINNING"])
-            hdr["BIASSEC"] = ("[1:%d,1:%d]" % (right_binned, hdr["NAXIS2"]), c1)
+            hdr["BIASSEC"] = ("[1:{:.0f},1:{}]".format(right_binned, hdr["NAXIS2"]), c1)
         elif img_top + img_height > top + height:
             top_binned = np.floor((is_bottom - hdr["YORGSUBF"]) / hdr["YBINNING"]) + 1
-            hdr["BIASSEC"] = ("[1:%d,%d:%d]" % (hdr["NAXIS1"], top_binned, hdr["NAXIS2"]), c1)
+            hdr["BIASSEC"] = ("[1:{},{:.0f}:{}]".format(hdr["NAXIS1"], top_binned, hdr["NAXIS2"]), c1)
         elif img_top < top:
             bottom_binned = np.ceil((is_top - hdr["YORGSUBF"]) / hdr["YBINNING"])
-            hdr["BIASSEC"] = ("[1:%d,1:%d]" % (hdr["NAXIS1"], bottom_binned), c1)
+            hdr["BIASSEC"] = ("[1:{},1:{:.0f}]".format(hdr["NAXIS1"], bottom_binned), c1)
 
     async def apply_meridian_flip(self, image: Image) -> None:
         """If the telescope has a meridian flip (MERIDIAN keyword in header), flip the image on both axes.

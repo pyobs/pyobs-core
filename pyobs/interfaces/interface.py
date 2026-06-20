e1ecc04b8b405515d11b316edd9c45a3f897fd6a
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-06-20 22:20:48 +0200

    added version=1 to Interface and Event classes

--- pyobs/interfaces/interface.py
+++ pyobs/interfaces/interface.py
@@ -4,6 +4,8 @@ from abc import ABCMeta
 class Interface(metaclass=ABCMeta):
     """Base class for all interfaces in pyobs."""
 
+    version: int = 1
+
     __module__ = "pyobs.interfaces"
     pass
 

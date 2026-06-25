242bf76ee5304af9311ef1a60f597b8f572e015e
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-06-25 12:49:41 +0200

    Remove unused `is_ready` method from `IReady` interface to simplify readiness handling

--- pyobs/interfaces/IReady.py
+++ pyobs/interfaces/IReady.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 from dataclasses import dataclass, field
-from typing import Any
 
 from ..utils.time import Time
 from .interface import Interface
@@ -17,15 +16,6 @@ class IReady(Interface, metaclass=ABCMeta):
     class State:
         ready: bool
         time: Time = field(default_factory=Time.now)
-
-    @abstractmethod
-    async def is_ready(self, **kwargs: Any) -> bool:
-        """Returns the device is "ready", whatever that means for the specific device.
-
-        Returns:
-            Whether device is ready
-        """
-        ...
 
 
 __all__ = ["IReady"]

5fc8e1fa844e9ae51da3d4f13db05731b2b3e970
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-06-30 15:59:33 +0200

    Remove unused import of `ILatLon` from `__init__.py` in `interfaces`

--- pyobs/interfaces/__init__.py
+++ pyobs/interfaces/__init__.py
@@ -34,7 +34,6 @@ from .IFocusModel import IFocusModel
 from .IGain import GainState, IGain
 from .IImageFormat import IImageFormat, ImageFormatCapabilities, ImageFormatState
 from .IImageType import IImageType, ImageTypeState
-from .ILatLon import ILatLon, LatLonCapabilities
 from .IMode import IMode, ModeCapabilities, ModeState
 from .IModule import IModule, ModuleCapabilities
 from .IMotion import DeviceMotionStatus, IMotion, MotionState
